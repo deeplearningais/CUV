ac43fb705e93052ef4a0469cc65fe808667911cd
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-14 15:03:38 +0000

    cuv: convert test case
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1456 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/convert.cpp
+++ src/tests/convert.cpp
@@ -31,6 +31,26 @@ BOOST_AUTO_TEST_CASE( create_dev_plain )
 	convert(hfr, dfc);
 }
 
+BOOST_AUTO_TEST_CASE( create_dev_plain2 )
+{
+	dev_dense_matrix<float,column_major> dfc(16,16); // "wrong" size
+	host_dense_matrix<float,row_major>  hfr(16,32);
+	convert(dfc, hfr);                               // should make dfc correct size
+	convert(hfr, dfc);
+	BOOST_CHECK( hfr.w() == dfc.h());
+	BOOST_CHECK( hfr.h() == dfc.w());
+}
+
+BOOST_AUTO_TEST_CASE( create_dev_plain3 )
+{
+	dev_dense_matrix<float,column_major> dfc(32,16); 
+	host_dense_matrix<float,row_major>  hfr(16,16);  // "wrong" size
+	convert(hfr, dfc);
+	convert(dfc, hfr);                               // should make dfc correct size
+	BOOST_CHECK( hfr.w() == dfc.h());
+	BOOST_CHECK( hfr.h() == dfc.w());
+}
+
 
 
 BOOST_AUTO_TEST_SUITE_END()
