84564d83ddc182c5534ab719f29bf772c8dd1fcc
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-15 22:23:37 +0000

    cuv: vec_ops tests: generic checks.
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1484 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/vec_op.cpp
+++ src/tests/vec_op.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
 #include <cuv_general.hpp>
 #include <dev_dense_matrix.hpp>
@@ -10,8 +11,9 @@ using namespace cuv;
 
 struct Fix{
 	dev_vector<float> v,w;
+	static const int N = 8092;
 	Fix()
-	:   v(256),w(256)
+	:   v(N),w(N)
 	{
 	}
 	~Fix(){
@@ -33,11 +35,11 @@ BOOST_AUTO_TEST_CASE( vec_ops_binary1 )
 	sequence(v);
 	sequence(w);
 	apply_scalar_functor(v,SF_ADD,1);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(v[i], i + 1);
 	}
 	apply_binary_functor(v,w, BF_ADD);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(v[i], i + i + 1);
 	}
 }
@@ -54,7 +56,7 @@ BOOST_AUTO_TEST_CASE( vec_ops_copy )
 
 	// copy data from v to w
 	copy(v,w);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(v[i],w[i]);
 	}
 }
@@ -63,7 +65,7 @@ BOOST_AUTO_TEST_CASE( vec_ops_unary_add )
 {
 	sequence(v);
 	apply_scalar_functor(v,SF_ADD,3.8f);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(v[i], i+3.8f);
 	}
 }
@@ -73,13 +75,13 @@ BOOST_AUTO_TEST_CASE( vec_ops_axpby )
 	sequence(w);
 	sequence(v);
 	apply_scalar_functor(v,SF_ADD, 1.f);
-	BOOST_CHECK_EQUAL(w[0],0);
-	BOOST_CHECK_EQUAL(v[0],1);
+	BOOST_CHECK_CLOSE(w[0],0.f,0.1f);
+	BOOST_CHECK_CLOSE(v[0],1.f,0.1f);
 
 	// copy data from v to w
 	apply_binary_functor(v,w,BF_AXPBY, 2.f,3.f);
-	for(int i=0;i<256;i++){
-		BOOST_CHECK_EQUAL(v[i], 2*(i+1) + 3*i );
+	for(int i=0;i<N;i++){
+		BOOST_CHECK_CLOSE(v[i], 2.f*(i+1.f) + 3.f*i, 0.1f );
 	}
 }
 
@@ -88,13 +90,13 @@ BOOST_AUTO_TEST_CASE( vec_ops_0ary1 )
 {
 	// test sequence
 	sequence(v);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(v[i], i);
 	}
 
 	// test fill
 	fill(w,1);
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		BOOST_CHECK_EQUAL(w[i], 1);
 	}
 }
@@ -104,13 +106,13 @@ BOOST_AUTO_TEST_CASE( vec_ops_norms )
 	sequence(v);
 	float f1 = norm1(v), f1_ = 0;
 	float f2 = norm2(v), f2_ = 0;
-	for(int i=0;i<256;i++){
+	for(int i=0;i<N;i++){
 		f2_ += v[i]*v[i];
 		f1_ += fabs(v[i]);
 	}
 	f2_ = sqrt(f2_);
-	BOOST_CHECK_EQUAL(f1,f1_);
-	BOOST_CHECK_EQUAL(f2,f2_);
+	BOOST_CHECK_CLOSE(f1,f1_,0.1f);
+	BOOST_CHECK_CLOSE(f2,f2_,0.1f);
 }
 
 
