37f9bec53be5532a2b2a0400593d027340a94f55
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-15 22:23:34 +0000

    cuv: matrix-ops for host matrices
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1481 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/matrix_ops/matrix_ops.hpp
+++ src/matrix_ops/matrix_ops.hpp
@@ -8,7 +8,7 @@
 namespace cuv{
 
   /****************************************
-   * Wrappers for Vector Ops 
+   * Wrappers for Vector Ops
    ****************************************
    */
   
@@ -20,6 +20,10 @@ namespace cuv{
 	  void apply_0ary_functor(dev_dense_matrix<V,M,I>& m, const NullaryFunctor& sf){ apply_0ary_functor(m.vec(),sf); }
   template<class V,class M,class I,class P> 
 	  void apply_0ary_functor(dev_dense_matrix<V,M,I>& m, const NullaryFunctor& sf, const P& param){apply_0ary_functor(m.vec(),sf,param);}
+  template<class V,class M,class I> 
+	  void apply_0ary_functor(host_dense_matrix<V,M,I>& m, const NullaryFunctor& sf){ apply_0ary_functor(m.vec(),sf); }
+  template<class V,class M,class I,class P> 
+	  void apply_0ary_functor(host_dense_matrix<V,M,I>& m, const NullaryFunctor& sf, const P& param){apply_0ary_functor(m.vec(),sf,param);}
 
   /*
    * Pointwise Unary Functor
@@ -27,6 +31,8 @@ namespace cuv{
    */
   template<class V,class M,class I> 
 	  void apply_scalar_functor(dev_dense_matrix<V,M,I>& m, const ScalarFunctor& sf){apply_scalar_functor(m.vec(),sf);}
+  template<class V,class M,class I> 
+	  void apply_scalar_functor(host_dense_matrix<V,M,I>& m, const ScalarFunctor& sf){apply_scalar_functor(m.vec(),sf);}
 
   /*
    * Pointwise Unary Functor with scalar parameter
@@ -34,6 +40,8 @@ namespace cuv{
    */
   template<class V,class M,class I,class P> 
 	  void apply_scalar_functor(dev_dense_matrix<V,M,I>& m, const ScalarFunctor& sf, const P& p) {apply_scalar_functor(m.vec(),sf,p);};
+  template<class V,class M,class I,class P> 
+	  void apply_scalar_functor(host_dense_matrix<V,M,I>& m, const ScalarFunctor& sf, const P& p) {apply_scalar_functor(m.vec(),sf,p);};
 
   /*
    * Pointwise Binary Functor
@@ -41,6 +49,8 @@ namespace cuv{
    */
   template<class V,class M,class I,class V2> 
 	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf){apply_binary_functor(v.vec(),w.vec(),bf);}
+  template<class V,class M,class I,class V2> 
+	  void apply_binary_functor(host_dense_matrix<V,M,I>& v, host_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf){apply_binary_functor(v.vec(),w.vec(),bf);}
 
   /*
    * Pointwise Binary Functor
@@ -48,6 +58,8 @@ namespace cuv{
    */
   template<class V,class M,class I,class V2,class P>
 	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param){apply_binary_functor(v.vec(),w.vec(),bf,param);}
+  template<class V,class M,class I,class V2,class P>
+	  void apply_binary_functor(host_dense_matrix<V,M,I>& v, host_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param){apply_binary_functor(v.vec(),w.vec(),bf,param);}
 
   /*
    * Pointwise Binary Functor
@@ -56,10 +68,15 @@ namespace cuv{
   template<class V,class M,class I,class V2,class P>
 	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param, const P& param2)
 	  {apply_binary_functor(v.vec(),w.vec(),bf,param,param2);}
+  template<class V,class M,class I,class V2,class P>
+	  void apply_binary_functor(host_dense_matrix<V,M,I>& v, host_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param, const P& param2)
+	  {apply_binary_functor(v.vec(),w.vec(),bf,param,param2);}
 
   // convenience wrappers
   template<class V,class M, class I>
 	  void copy(dev_dense_matrix<V,M,I>& dst, dev_dense_matrix<V,M,I>& src){ apply_binary_functor(dst.vec(),src.vec(),BF_COPY); }
+  template<class V,class M, class I>
+	  void copy(host_dense_matrix<V,M,I>& dst, host_dense_matrix<V,M,I>& src){ apply_binary_functor(dst.vec(),src.vec(),BF_COPY); }
 
 
   /*
@@ -70,6 +87,11 @@ namespace cuv{
   template<class V,class M, class I> float norm1(dev_dense_matrix<V,M,I>& v){return norm1(v.vec());}
   template<class V,class M, class I> float mean(dev_dense_matrix<V,M,I>& v) {return mean(v.vec());}
   template<class V,class M, class I> float var(dev_dense_matrix<V,M,I>& v)  {return var(v.vec());}
+
+  template<class V,class M, class I> float norm2(host_dense_matrix<V,M,I>& v){return norm2(v.vec());}
+  template<class V,class M, class I> float norm1(host_dense_matrix<V,M,I>& v){return norm1(v.vec());}
+  template<class V,class M, class I> float mean(host_dense_matrix<V,M,I>& v) {return mean(v.vec());}
+  template<class V,class M, class I> float var(host_dense_matrix<V,M,I>& v)  {return var(v.vec());}
 
 
 } // cuv
