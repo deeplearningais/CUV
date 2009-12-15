8f89b504d89fa97f08c698de3a744b4948e7f8d7
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-15 13:57:05 +0000

    cuv: add vector-based matrix op wrappers
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1475 9a9a52a9-7681-4bea-9213-143ca318e872

---
+++ src/matrix_ops/matrix_ops.hpp
@@ -0,0 +1,77 @@
+#ifndef __MATRIX_OPS_HPP__
+#define __MATRIX_OPS_HPP__
+
+#include <vector_ops.hpp>
+#include <dev_dense_matrix.hpp>
+#include <host_dense_matrix.hpp>
+
+namespace cuv{
+
+  /****************************************
+   * Wrappers for Vector Ops 
+   ****************************************
+   */
+  
+  /*
+   * Pointwise Null-ary Functor
+   * v = sf()
+   */
+  template<class V,class M,class I> 
+	  void apply_0ary_functor(dev_dense_matrix<V,M,I>& m, const NullaryFunctor& sf){ apply_0ary_functor(m.vec(),sf); }
+  template<class V,class M,class I,class P> 
+	  void apply_0ary_functor(dev_dense_matrix<V,M,I>& m, const NullaryFunctor& sf, const P& param){apply_0ary_functor(m.vec(),sf,param);}
+
+  /*
+   * Pointwise Unary Functor
+   * v = sf(v)
+   */
+  template<class V,class M,class I> 
+	  void apply_scalar_functor(dev_dense_matrix<V,M,I>& m, const ScalarFunctor& sf){apply_scalar_functor(m.vec(),sf);}
+
+  /*
+   * Pointwise Unary Functor with scalar parameter
+   * v = sf(v, param)
+   */
+  template<class V,class M,class I,class P> 
+	  void apply_scalar_functor(dev_dense_matrix<V,M,I>& m, const ScalarFunctor& sf, const P& p) {apply_scalar_functor(m.vec(),sf,p);};
+
+  /*
+   * Pointwise Binary Functor
+   * v = bf(v,w)
+   */
+  template<class V,class M,class I,class V2> 
+	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf){apply_binary_functor(v.vec(),w.vec(),bf);}
+
+  /*
+   * Pointwise Binary Functor
+   * v = bf(v,w,param)
+   */
+  template<class V,class M,class I,class V2,class P>
+	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param){apply_binary_functor(v.vec(),w.vec(),bf,param);}
+
+  /*
+   * Pointwise Binary Functor
+   * v = bf(v,w,param,param2)
+   */
+  template<class V,class M,class I,class V2,class P>
+	  void apply_binary_functor(dev_dense_matrix<V,M,I>& v, dev_dense_matrix<V2,M,I>& w, const BinaryFunctor& bf, const P& param, const P& param2)
+	  {apply_binary_functor(v.vec(),w.vec(),bf,param,param2);}
+
+  // convenience wrappers
+  template<class V,class M, class I>
+	  void copy(dev_dense_matrix<V,M,I>& dst, dev_dense_matrix<V,M,I>& src){ apply_binary_functor(dst.vec(),src.vec(),BF_COPY); }
+
+
+  /*
+   * reductions
+   *
+   */
+  template<class V,class M, class I> float norm2(dev_dense_matrix<V,M,I>& v){return norm2(v.vec());}
+  template<class V,class M, class I> float norm1(dev_dense_matrix<V,M,I>& v){return norm1(v.vec());}
+  template<class V,class M, class I> float mean(dev_dense_matrix<V,M,I>& v) {return mean(v.vec());}
+  template<class V,class M, class I> float var(dev_dense_matrix<V,M,I>& v)  {return var(v.vec());}
+
+
+} // cuv
+
+#endif
---
+++ src/tests/matrix_op.cpp
@@ -0,0 +1,118 @@
+#define BOOST_TEST_MODULE example
+#include <boost/test/included/unit_test.hpp>
+
+#include <cuv_general.hpp>
+#include <dev_dense_matrix.hpp>
+#include <host_dense_matrix.hpp>
+#include <matrix_ops.hpp>
+
+using namespace cuv;
+
+struct Fix{
+	dev_dense_matrix<float> v,w;
+	Fix()
+	:   v(256,256),w(256,256)
+	{
+	}
+	~Fix(){
+	}
+};
+
+
+BOOST_FIXTURE_TEST_SUITE( s, Fix )
+
+
+BOOST_AUTO_TEST_CASE( vec_ops_unary1 )
+{
+	apply_scalar_functor(v, SF_EXP);
+	apply_scalar_functor(v, SF_EXACT_EXP);
+}
+
+BOOST_AUTO_TEST_CASE( vec_ops_binary1 )
+{
+	sequence(v.vec());
+	sequence(w.vec());
+	apply_scalar_functor(v,SF_ADD,1);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i], i + 1);
+	}
+	apply_binary_functor(v,w, BF_ADD);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i], i + i + 1);
+	}
+}
+
+BOOST_AUTO_TEST_CASE( vec_ops_binary2 )
+{
+	apply_binary_functor(v,w, BF_AXPY, 2);
+}
+
+BOOST_AUTO_TEST_CASE( vec_ops_copy )
+{
+	// generate data
+	sequence(w.vec());
+
+	// copy data from v to w
+	copy(v,w);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i],w.vec()[i]);
+	}
+}
+
+BOOST_AUTO_TEST_CASE( vec_ops_unary_add )
+{
+	sequence(v.vec());
+	apply_scalar_functor(v,SF_ADD,3.8f);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i], i+3.8f);
+	}
+}
+BOOST_AUTO_TEST_CASE( vec_ops_axpby )
+{
+	// generate data
+	sequence(w);
+	sequence(v);
+	apply_scalar_functor(v,SF_ADD, 1.f);
+	BOOST_CHECK_EQUAL(w.vec()[0],0);
+	BOOST_CHECK_EQUAL(v.vec()[0],1);
+
+	// copy data from v to w
+	apply_binary_functor(v,w,BF_AXPBY, 2.f,3.f);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i], 2*(i+1) + 3*i );
+	}
+}
+
+
+BOOST_AUTO_TEST_CASE( vec_ops_0ary1 )
+{
+	// test sequence
+	sequence(v.vec());
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(v.vec()[i], i);
+	}
+
+	// test fill
+	fill(w.vec(),1);
+	for(int i=0;i<256;i++){
+		BOOST_CHECK_EQUAL(w.vec()[i], 1);
+	}
+}
+
+BOOST_AUTO_TEST_CASE( vec_ops_norms )
+{
+	sequence(v.vec());
+	float f1 = norm1(v), f1_ = 0;
+	float f2 = norm2(v), f2_ = 0;
+	for(int i=0;i<256;i++){
+		f2_ += v.vec()[i]*v.vec()[i];
+		f1_ += fabs(v.vec()[i]);
+	}
+	f2_ = sqrt(f2_);
+	BOOST_CHECK_EQUAL(f1,f1_);
+	BOOST_CHECK_EQUAL(f2,f2_);
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
