779583ae15f1cc399213da5a548e9e433752e4c1
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-22 10:38:08 +0000

    cuv: minor.
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1508 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/random.cpp
+++ src/tests/random.cpp
@@ -49,26 +49,26 @@ BOOST_AUTO_TEST_CASE( random_normal )
 	BOOST_CHECK_SMALL( m, 0.02f );
 	BOOST_CHECK_SMALL( std-1.f, 0.01f );
 
-	m   = mean(v);
-	std = std::sqrt(var(v));
+	m   = mean(x);
+	std = std::sqrt(var(x));
 	BOOST_CHECK_SMALL( m, 0.02f );
 	BOOST_CHECK_SMALL( std-1.f, 0.01f );
 }
 BOOST_AUTO_TEST_CASE( binarize )
 {
-	fill_rnd_normal(v);
-	fill_rnd_normal(x);
+	fill_rnd_uniform(v);
+	fill_rnd_uniform(x);
 	rnd_binarize(v);
 	rnd_binarize(x);
 
 	float m   = mean(v);
-	BOOST_CHECK_SMALL( m, 0.5f );
+	BOOST_CHECK_SMALL( m-0.5f, 0.01f );
 	for(int i = 0; i < n; ++ i) {
 		BOOST_CHECK( v[i] == 0  || v[i] == 1 );
 	}
 
-	m   = mean(v);
-	BOOST_CHECK_SMALL( m, 0.5f );
+	m   = mean(x);
+	BOOST_CHECK_SMALL( m-0.5f, 0.01f );
 	for(int i = 0; i < n; ++ i) {
 		BOOST_CHECK( v[i] == 0  || v[i] == 1 );
 	}
