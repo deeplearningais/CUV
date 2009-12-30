7f0a0bdcf52d2da957f3eef33636b1f4bb41a4f8
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2009-12-30 13:28:34 +0000

    cuv: "set" function to change single elements of dense matrices
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1518 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/basics/host_dense_matrix.hpp
+++ src/basics/host_dense_matrix.hpp
@@ -24,6 +24,8 @@ namespace cuv{
 			inline const value_type operator()(const index_type& i, const index_type& j, row_major)    const;
 			inline       value_type operator()(const index_type& i, const index_type& j, column_major) ;
 			inline       value_type operator()(const index_type& i, const index_type& j, row_major)    ;
+			inline		 void set(index_type i, index_type j, value_type val, column_major);
+			inline		 void set(index_type i, index_type j, value_type val, row_major);
 		public:
 			/*
 			 * Member access
@@ -38,6 +40,7 @@ namespace cuv{
 			inline       vec_type& vec()        { return *m_vec; }
 			inline const vec_type* vec_ptr()  const { return m_vec; }
 			inline       vec_type* vec_ptr()        { return m_vec; }
+			inline 		 void set(index_type i, index_type j, value_type val);
 
 			/*
 			 * Life cycle
@@ -123,6 +126,23 @@ namespace cuv{
 			typedef host_memory_space memory_space_type;
 			typedef M                 memory_layout_type;
 		};
+
+	/*
+	 * Change values in dense host matrix
+	 *
+	 */
+	template<class V, class M, class I>
+	void
+	host_dense_matrix<V,M,I>::set(index_type i, index_type j, value_type val, column_major) { (*m_vec)[ this->h()*j + i] = val; };
+
+	template<class V, class M, class I>
+	void
+	host_dense_matrix<V,M,I>::set(index_type i, index_type j, value_type val, row_major) { (*m_vec)[ this->w()*i + j] = val; };
+
+	template<class V, class M, class I>
+	void
+	host_dense_matrix<V,M,I>::set(index_type i, index_type j, value_type val) { this->set(i, j, val, memory_layout()); };
+
 
 }
 
