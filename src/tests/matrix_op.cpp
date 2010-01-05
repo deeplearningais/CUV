418b62c5afe84a32bf9d973115bacffedb027420
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-01-05 14:49:27 +0000

    cuv: Test for matrix product with (1xN) matrices
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1519 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/matrix_op.cpp
+++ src/tests/matrix_op.cpp
@@ -14,11 +14,11 @@ using namespace cuv;
 struct Fix{
 	static const int n=256;
 	static const int N=n*n;
-	dev_dense_matrix<float> u,v,w;
-	host_dense_matrix<float> r,x,z;
+	dev_dense_matrix<float> a,b,u,v,w;
+	host_dense_matrix<float> s,t,r,x,z;
 	Fix()
-	:   u(n,n),v(n,n),w(n,n)
-	,   r(n,n),x(n,n),z(n,n)
+	:   a(1,n),b(1,n),u(n,n),v(n,n),w(n,n)
+	,   s(1,n),t(1,n),r(n,n),x(n,n),z(n,n)
 	{
 	}
 	~Fix(){
@@ -141,6 +141,27 @@ BOOST_AUTO_TEST_CASE( mat_op_mm )
 	}
 }
 
+BOOST_AUTO_TEST_CASE( mat_op_mmdim1 )
+{
+	sequence(a);     apply_scalar_functor(a, SF_MULT, 0.01f);
+	sequence(w);     apply_scalar_functor(w, SF_MULT, 0.01f);
+	sequence(s);     apply_scalar_functor(s, SF_MULT, 0.01f);
+	sequence(z);     apply_scalar_functor(z, SF_MULT, 0.01f);
+	prod(b,a,w,'n','t');
+	prod(t,s,z,'n','t');
+
+	host_dense_matrix<float> b2(b.h(), b.w());
+	convert(b2,b);
+
+	for(int i=0;i<256;i++) {
+		float val = 0.0f;
+		for(int j=0;j<256;j++) {
+			val += s(0,j) * z(i,j);
+		}
+		BOOST_CHECK_CLOSE( b2(0,i), val, 0.01 );
+		BOOST_CHECK_CLOSE( t(0,i), val, 0.01 );
+	}
+}
 
 
 BOOST_AUTO_TEST_SUITE_END()
