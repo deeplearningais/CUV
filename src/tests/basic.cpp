15f6e2ef5a214e118604786075a77bebabaf9dc1
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-01-07 16:30:41 +0000

    cuv: test for vector set() function
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1536 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/basic.cpp
+++ src/tests/basic.cpp
@@ -8,7 +8,13 @@
 using namespace cuv;
 
 struct Fix{
-	Fix(){
+	static const int N=256;
+	dev_vector<float> v;
+	host_vector<float> w;
+	Fix()
+	:   v(N)
+	,   w(N)
+	{
 	}
 	~Fix(){
 	}
@@ -39,6 +45,18 @@ BOOST_AUTO_TEST_CASE( create_host )
 {
 	host_dense_matrix<float> m(16,16);
 	host_dense_matrix<float> m2(16,16,new host_vector<float>(m.n(),m.ptr(),true));
+}
+
+BOOST_AUTO_TEST_CASE( set_vector_elements )
+{
+	for(int i=0; i < N; i++) {
+		v.set(i, (float) i/N);
+		w.set(i, (float) i/N);
+	}
+	for(int i=0; i < N; i++) {
+		BOOST_CHECK_EQUAL(v[i], (float) i/N );
+		BOOST_CHECK_EQUAL(w[i], (float) i/N );
+	}
 }
 
 
