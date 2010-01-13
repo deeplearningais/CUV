d666b8afc5ac542102ad03804b6cbab322aafb02
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-01-13 16:36:29 +0000

    (cuv) Tests for convolution operations
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1617 9a9a52a9-7681-4bea-9213-143ca318e872

---
+++ src/tests/conv_op.cpp
@@ -0,0 +1,75 @@
+#define BOOST_TEST_MODULE example
+#include <cstdio>
+#include <boost/test/included/unit_test.hpp>
+
+#include <cuv_general.hpp>
+#include <dev_dense_matrix.hpp>
+#include <host_dense_matrix.hpp>
+#include <vector_ops.hpp>
+#include <matrix_ops.hpp>
+#include <convolution_ops.hpp>
+#include <timing.hpp>
+#include <random.hpp>
+#include <matrix_ops/rprop.hpp>
+#include <convert.hpp>
+
+using namespace cuv;
+
+struct MyConfig {
+	static const int dev = 0;
+	MyConfig()   {
+		printf("Testing on device=%d\n",dev);
+		initCUDA(dev);
+	}
+	~MyConfig()  { exitCUDA();  }
+};
+
+
+BOOST_GLOBAL_FIXTURE( MyConfig );
+
+
+struct Fix{
+	static const int c = 4;  // # patterns
+	static const int n = 128;  // image size
+	static const int f = 32;   // # filters
+	static const int g = 16;    // filter size
+	static const int k = n-g+1;// target image size
+	dev_dense_matrix<float, row_major>  d_img,d_filter,d_dst;
+	host_dense_matrix<float, row_major> h_img,h_filter,h_dst;
+	Fix()
+	:   d_img(c,n*n), d_filter(f,g*g), d_dst(c,f*k*k)
+	,   h_img(c,n*n), h_filter(f,g*g), h_dst(c,f*k*k)
+	{
+		//MEASURE_TIME("warmup", apply_scalar_functor(v, SF_EXP), 100);
+	}
+	~Fix(){
+	}
+};
+
+
+BOOST_FIXTURE_TEST_SUITE( s, Fix )
+
+BOOST_AUTO_TEST_CASE( convolution )
+{
+	fill(d_dst, 0.0f);
+	sequence(d_img);    apply_scalar_functor(d_img,   SF_MULT,0.001f);
+	sequence(d_filter); apply_scalar_functor(d_filter,SF_MULT,0.001f);
+
+	fill(h_dst, 0.0f);
+	sequence(h_img);    apply_scalar_functor(h_img,   SF_MULT,0.001f);
+	sequence(h_filter); apply_scalar_functor(h_filter,SF_MULT,0.001f);
+
+	convolve(d_dst,d_img,d_filter);
+	convolve(h_dst,h_img,h_filter);
+
+	host_dense_matrix<float, row_major> dst2(d_dst.h(), d_dst.w());
+	convert(dst2,d_dst);
+
+	for(int i=0;i<d_dst.h();i++){
+		for(int j=0;j<d_dst.w();j++){
+			BOOST_CHECK_CLOSE( dst2(i,j), h_dst(i,j), 0.001 );
+		}
+	}
+}
+
+}
---
+++ src/tests/conv_op_speed.cpp
@@ -0,0 +1,94 @@
+#define BOOST_TEST_MODULE example
+#include <cstdio>
+#include <boost/test/included/unit_test.hpp>
+
+#include <cuv_general.hpp>
+#include <dev_dense_matrix.hpp>
+#include <host_dense_matrix.hpp>
+#include <vector_ops.hpp>
+#include <matrix_ops.hpp>
+#include <convolution_ops.hpp>
+#include <timing.hpp>
+#include <random.hpp>
+#include <matrix_ops/rprop.hpp>
+#include <convert.hpp>
+
+using namespace cuv;
+
+#define MEASURE_TIME(MSG, OPERATION, ITERS)     \
+	float MSG;                                  \
+	if(1){                                      \
+		Timing tim;                             \
+		for(int i=0;i<ITERS;i++){               \
+			OPERATION ;                         \
+		}                                       \
+		tim.update(ITERS);                      \
+		printf("%s [%s] took %4.4f us/pass\n", #MSG, #OPERATION, 1000000.0f*tim.perf()); \
+		MSG = 1000000.0f*tim.perf();            \
+	}
+
+struct MyConfig {
+	static const int dev = 0;
+	MyConfig()   { 
+		printf("Testing on device=%d\n",dev);
+		initCUDA(dev); 
+	}
+	~MyConfig()  { exitCUDA();  }
+};
+
+
+BOOST_GLOBAL_FIXTURE( MyConfig );
+
+
+struct Fix{
+	Fix()
+	{
+	}
+	~Fix(){
+	}
+};
+
+
+BOOST_FIXTURE_TEST_SUITE( s, Fix )
+
+void conv_speed_test(int inputSize, int filterSize, int numFilters, int numImages)
+{
+	int c = numImages;
+	int n = inputSize;
+	int f = numFilters;
+	int g = filterSize;
+	int k = inputSize-filterSize+1;
+
+	printf("Convolving %i images of size %ix%i with %i filters of size %ix%i.\n", c, n,n, f, g,g);
+	printf("Result are %i*%i images of size %ix%i\n", c,f, k,k);
+
+	dev_dense_matrix<float, row_major>  d_img(c, n*n);
+	dev_dense_matrix<float, row_major>  d_filter(f, g*g);
+	dev_dense_matrix<float, row_major>  d_dst(c, f*k*k);
+
+	host_dense_matrix<float, row_major>  h_img(c, n*n);
+	host_dense_matrix<float, row_major>  h_filter(f, g*g);
+	host_dense_matrix<float, row_major>  h_dst(c, f*k*k);
+
+	fill(d_dst, 0.0f);
+	sequence(d_img);    apply_scalar_functor(d_img,   SF_MULT,0.001f);
+	sequence(d_filter); apply_scalar_functor(d_filter,SF_MULT,0.001f);
+
+	fill(h_dst, 1.0f);
+	sequence(h_img);    apply_scalar_functor(h_img,   SF_MULT,0.001f);
+	sequence(h_filter); apply_scalar_functor(h_filter,SF_MULT,0.001f);
+
+	MEASURE_TIME(dev,  convolve(d_dst,d_img,d_filter), 10);
+	MEASURE_TIME(host, convolve(h_dst,h_img,h_filter), 10);
+
+	printf("Speedup: %3.4f\n", host/dev);
+}
+
+BOOST_AUTO_TEST_CASE( convolution_speed )
+{
+	conv_speed_test(140, 16, 16, 30);
+	conv_speed_test(40, 9, 16, 128);
+	conv_speed_test(47, 15, 16, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
