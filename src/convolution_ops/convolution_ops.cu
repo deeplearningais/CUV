f355b7264cd7e94910c84df5984d0ade1ba55cea
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-01-13 16:35:51 +0000

    (cuv) Fix: Synchronize threads after device convolution
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1616 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/convolution_ops/convolution_ops.cu
+++ src/convolution_ops/convolution_ops.cu
@@ -28,6 +28,7 @@ template<>
 
 	// execute convolution
 	convolve_bw(&nv_img, &nv_filter, &nv_dst);
+	cuvSafeCall(cudaThreadSynchronize());
 	}
 
 /*
@@ -53,48 +54,23 @@ void convolve(host_dense_matrix<float,row_major>& dst,
 	int imgPixels = imgSize * imgSize;
 	int filterPixels = filterSize * filterSize;
 
+// Alex' host convolution is ~25% faster due to better memory access patterns
+//	convCPU(img.ptr(), filter.ptr(), dst.ptr(), imgSize, filterSize, numImages, numFilters);
+
 	for(int i=0; i<numImages; i++)
-		for(int r=0; r<imgSize; r++)
-			for(int c=0; c<imgSize; c++)
-				for(int f=0; f<numFilters; f++) {
+		for(int f=0; f<numFilters; f++)
+			for(int r=0; r<dstSize; r++)
+				for(int c=0; c<dstSize; c++) {
 					float sum = 0.0f;
-					for(int y=0; y<filterSize; y++)
+					for(int y=0; y<filterSize; y++) {
+						float subsum = 0.0f;
 						for(int x=0; x<filterSize; x++)
-							sum += img(i, (r+y)*imgSize + (c+x) ) * filter(f, y * filterSize + x);
+							subsum += img(i, (r+y)*imgSize + (c+x) ) *  filter(f, y * filterSize + x);
+						sum += subsum;
+					}
+					sum += dst(i, f*dstPixels + r*dstSize + c);
 					dst.set(i, f*dstPixels + r*dstSize + c, sum);
 				}
-
-	/* obsolete code of host convolution utilizing cuda devices
-
-	// some preliminary checks to ensure compatibility
-	int numFilters = filter.h();
-	int filterSize = sqrt(filter.w());
-	int imgSize = sqrt(img.w());
-	int dstSize = sqrt(dst.w()/numFilters);
-	cuvAssert(numFilters%16 == 0);
-	cuvAssert(filterSize*filterSize == filter.w());
-	cuvAssert(imgSize*imgSize == img.w());
-	cuvAssert(dstSize == imgSize - filterSize + 1);
-
-	// make copies of the host matrices on the device
-	dev_dense_matrix<float,row_major> d_img(img.h(),img.w());
-	convert(d_img, img);
-	dev_dense_matrix<float,row_major> d_dst(dst.h(),dst.w());
-	fill(d_dst.vec(), 0.0f);
-	dev_dense_matrix<float,row_major> d_filter(filter.h(),filter.w());
-	convert(d_filter, filter);
-
-	// make NVMatrices with this data
-	NVMatrix nv_dst(d_dst.ptr(), d_dst.h(), d_dst.w(), false);
-	NVMatrix nv_img(d_img.ptr(), d_img.h(), d_img.w(), false);
-	NVMatrix nv_filter(d_filter.ptr(), d_filter.h(), d_filter.w(), false);
-
-	// execute convolution
-	convolve_bw(&nv_img, &nv_filter, &nv_dst);
-
-	// convert back
-	convert(dst, d_dst);
-	*/
 }
 
 }
