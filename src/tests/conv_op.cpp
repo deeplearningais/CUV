625da324bb09d7793681b151d9f5729049573600
scherer@AIS.INFORMATIK.UNI-BONN.DE <scherer@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-01-14 16:13:57 +0000

    (cuv) Test for local maximum
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1644 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/conv_op.cpp
+++ src/tests/conv_op.cpp
@@ -16,16 +16,18 @@
 using namespace cuv;
 
 struct Fix{
-	static const int c = 2;  // # patterns
+	static const int c = 2;  // # patterns (images)
 	static const int n = 64;  // image size
 	static const int f = 16;   // # filters
+	static const int p = 8;	   // pooling size
 	static const int g = 8;    // filter size
 	static const int k = n-g+1;// target image size
-	dev_dense_matrix<float, row_major>  d_img,d_filter,d_dst;
-	host_dense_matrix<float, row_major> h_img,h_filter,h_dst;
+	static const int o = n/p;  // pooling output size
+	dev_dense_matrix<float, row_major>  d_img,d_filter,d_dst,d_pooled;
+	host_dense_matrix<float, row_major> h_img,h_filter,h_dst,h_pooled;
 	Fix()
-	:   d_img(c,n*n), d_filter(f,g*g), d_dst(c,f*k*k)
-	,   h_img(c,n*n), h_filter(f,g*g), h_dst(c,f*k*k)
+	:   d_img(c,n*n), d_filter(f,g*g), d_dst(c,f*k*k), d_pooled(c,o*o)
+	,   h_img(c,n*n), h_filter(f,g*g), h_dst(c,f*k*k), h_pooled(c,o*o)
 	{
 		//MEASURE_TIME("warmup", apply_scalar_functor(v, SF_EXP), 100);
 	}
@@ -59,4 +61,24 @@ BOOST_AUTO_TEST_CASE( convolution )
 	}
 }
 
+BOOST_AUTO_TEST_CASE( local_maximum )
+{
+	fill(d_dst, 0.0f);
+	sequence(d_img);    apply_scalar_functor(d_img,   SF_MULT,0.001f);
+
+	fill(h_dst, 0.0f);
+	sequence(h_img);    apply_scalar_functor(h_img,   SF_MULT,0.001f);
+
+	localMaximum(h_pooled, h_img, p);
+	localMaximum(d_pooled, d_img, p);
+
+	host_dense_matrix<float, row_major> pooled2(d_pooled.h(), d_pooled.w());
+	convert(pooled2,d_pooled);
+
+	for(int i=0;i<d_pooled.h();i++){
+		for(int j=0;j<d_pooled.w();j++){
+			BOOST_CHECK_CLOSE( pooled2(i,j), h_pooled(i,j), 0.001 );
+		}
+	}
+}
 }
