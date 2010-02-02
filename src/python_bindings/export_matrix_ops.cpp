06b2a36d3056dfad8ceb3853393c0092aa0b5864
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-02-02 19:43:14 +0000

    cuv: default arg for null-ptr must be object() in boost.python
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@1940 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/python_bindings/export_matrix_ops.cpp
+++ src/python_bindings/export_matrix_ops.cpp
@@ -149,8 +149,8 @@ void export_binary_functor() {
 template <class M>
 void export_pooling(){
 	typedef typename switch_value_type<M,int>::type Mint;
-	def("max_pool",(void (*)(M&,M&,int,Mint*))local_maximum<typename M::value_type,typename M::memory_layout,typename M::index_type>, (arg("dst"),arg("img"),arg("poolSize"),arg("optional_indices")=NULL));
-	def("supersample",(void (*)(M&,M&,int,Mint*))supersample<typename M::value_type,typename M::memory_layout,typename M::index_type>,(arg("dst"),arg("img"),arg("factor"),arg("optional_indices")=NULL));
+	def("max_pool",(void (*)(M&,M&,int,Mint*))local_maximum<typename M::value_type,typename M::memory_layout,typename M::index_type>, (arg("dst"),arg("img"),arg("poolSize"),arg("optional_indices")=object()));
+	def("supersample",(void (*)(M&,M&,int,Mint*))supersample<typename M::value_type,typename M::memory_layout,typename M::index_type>,(arg("dst"),arg("img"),arg("factor"),arg("optional_indices")=object()));
 }
 
 template <class M>
