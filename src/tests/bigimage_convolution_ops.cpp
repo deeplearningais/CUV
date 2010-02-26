d2a206ce23e11366a6f2dc3ff66b17c714cf7461
gerharda@AIS.INFORMATIK.UNI-BONN.DE <gerharda@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-02-26 15:53:05 +0000

    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@3192 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/tests/bigimage_convolution_ops.cpp
+++ src/tests/bigimage_convolution_ops.cpp
@@ -22,18 +22,18 @@
 using namespace cuv;
 
 struct Fix{
-	static const int c = 1;  // # patterns (images)
+	static const int c = 2;  // # patterns (images)
 	static const int n = 1024;  // image size
 	static const int f = 16;   // # filters
 	static const int p = 8;	   // pooling size
 	static const int g = 8;    // filter size
-	static const int k = n-g+1;// target image size
+	static const int k = (n-g+1);//n-g+1;// target image size
 	static const int o = n/p;  // pooling output size
 	dev_dense_matrix<float, row_major>  d_img,d_filter,d_dst,d_pooled;
 	host_dense_matrix<float, row_major> h_img,h_filter,h_dst,h_pooled;
 	Fix()
-		:   d_img(c,n*n), d_filter(f,g*g), d_dst(c,f*k*k), d_pooled(c,o*o)
-		,   h_img(c,n*n), h_filter(f,g*g), h_dst(c,f*k*k), h_pooled(c,o*o)
+		:   d_img(c,n*n), d_filter(c,f*g*g), d_dst(c,f*k*k), d_pooled(c,o*o)
+		,   h_img(c,n*n), h_filter(c,f*g*g), h_dst(c,f*k*k), h_pooled(c,o*o)
 	{
 			//MEASURE_TIME("warmup", apply_scalar_functor(v, SF_EXP), 100);
 	}
@@ -50,73 +50,106 @@ BOOST_AUTO_TEST_CASE( blur )
 /*
  * Set up matrices - insert 1 pic 2 times in Matrix
  */
-	char fileName[] = "/home/VI/stud/gerharda/lhome/workspace/SimpleCUDATestcase/build/hubble.png";
-	IplImage* img = cvLoadImage(fileName,0);
-	if(!img) std::printf("Could not load image file: %s\n",fileName);
-	int img_size = img->width*img->height*img->nChannels;
+	std::vector<char*> fileName;
+	fileName.push_back("/home/VI/stud/gerharda/Desktop/png-segments/001/frame000000_cam0.png");
+	fileName.push_back("/home/VI/stud/gerharda/Desktop/png-segments/001/frame000001_cam0.png");
 
-	// TODO: what if h_img.w() != img->width()?
+	IplImage* img[2];
+	img[0] = cvLoadImage(fileName[0],0);
+	if(!img) std::printf("Could not load image file: %s\n",fileName[0]);
+
+	img[1] = cvLoadImage(fileName[1], 0);
+	if(!img) std::printf("Could not load image file: %s\n",fileName[1]);
+
+	int img_size = img[0]->width*img[0]->height*img[0]->nChannels;
+
+	// TODO: make sure that h_img.w() == img->width()?
 	float* h_vec = h_img.ptr();
 	float* d_vec = d_img.ptr();
-	char* le_ptr = img->imageData;
+
+	char* le_ptr = img[0]->imageData;
 	for (int i=0; i < img_size; i++ ){
 		*h_vec++ = (float) (*le_ptr++);
-		//*d_vec++ = (float) (*le_ptr++);
 	}
-//	le_ptr = img->imageData;
-//	for (int i=0; i < img_size; i++ ){
-//		*h_vec++ = (float) (*le_ptr++);
-//		//*d_vec++ = (float) (*le_ptr++);
-//	}
+	le_ptr = img[1]->imageData;
+	for (int i=0; i < img_size; i++ ){
+		*h_vec++ = (float) (*le_ptr++);
+	}
 	convert(d_img, h_img);
-
 /*
  * Set up filters
  */
 
 	fill(d_filter.vec(), 0.0f);
 	fill(h_filter.vec(), 0.0f);
-
+	fill(d_dst.vec(), 0.0f);
 	// in the middle of each filter i want to be a 1
-	for (unsigned int i=p*p/2;
-			i < p*p*f;
-			i= i + p*p/2){
-		//*(d_filter.ptr() + i) = 1;
+	for (unsigned int i=g*g/2;
+			i < c*g*g*f;
+			i= i + g*g/2){
 		*(h_filter.ptr() + i) = 1;
 	}
 	convert(d_filter,h_filter);
 
-
+//	NVMatrix nv_dst(dst.ptr(), dst.h(), dst.w(), false);
+//	NVMatrix nv_img(img.ptr(), img.h(), img.w(), false);
+//	NVMatrix nv_filter(filter.ptr(), filter.h(), filter.w(), false);
 
 //	std::cout << h_filter << std::endl;
 
-    convolve(d_dst,d_img,d_filter);
+    convolve2(d_dst,d_img,d_filter, f);
 //	convolve(h_dst,h_img,h_filter);
 //
 	host_dense_matrix<float, row_major> dst2(d_dst.h(), d_dst.w());
     convert(dst2,d_dst);
 
     //cvNamedWindow("org", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("transformed", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("transformed 0", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("transformed 1", CV_WINDOW_AUTOSIZE);
+
     //cvShowImage("org", img);
-    char* char_ptr = img->imageData;
+
+    img[0]->width = k;
+    img[0]->height = k;
+    img[0]->imageSize = k*k;
+    img[0]->widthStep = k;
+
+    img[1]->width = k;
+    img[1]->height = k;
+    img[1]->imageSize = k*k;
+    img[1]->widthStep = k;
+
+    // read back and show pic 0
+    char* char_ptr = img[0]->imageData;
+    memset(char_ptr, 0, img_size);
     float* dst2_ptr = dst2.ptr();
-    for (int i=0; i < img_size; i++ ){
-    	(*char_ptr++) = (char) ((*dst2_ptr++));
+    for (int i=0; i < k*k; i++ ){
+    	(*char_ptr++) = floor(((*dst2_ptr++)));
     }
-    cvShowImage("transformed", img);
 
-
-    cvSaveImage("./filtered.png", img);
+    cvSaveImage("./filtered0.png", img[0]);
+    cvShowImage("transformed 0", img[0]);
     cvWaitKey(0);
-    cvReleaseImage( &img );
+    // read back and show pic 1
+    char_ptr = img[1]->imageData;
+    memset(char_ptr, 0, img_size);
+
+        //float* dst2_ptr = dst2.ptr();
+    for (int i=0; i < k*k; i++ ){
+        	(*char_ptr++) = floor(((*dst2_ptr++)));
+        }
+    //cvShowImage("transformed 1", img[1]);
+
+//    cvSaveImage("./filtered0.png", img[0]);
+//    cvSaveImage("./filtered1.png", img[1]);
+    cvWaitKey(0);
+    cvReleaseImage( &img[0] );
+    cvReleaseImage( &img[1] );
     //cvDestroyWindow( "org" );
-    cvDestroyWindow( "transformed" );
+    cvDestroyWindow( "transformed 0" );
+    cvDestroyWindow( "transformed 1" );
 
 }
-
-
-
 
 BOOST_AUTO_TEST_SUITE_END()
 
