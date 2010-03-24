25fc7927c7387f19b9283e3d26addf9e28911691
schulz@AIS.INFORMATIK.UNI-BONN.DE <schulz@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-03-24 22:08:27 +0000

    cuv: adjust python-export of super_to_max
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@3386 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/python_bindings/export_convolution_ops.cpp
+++ src/python_bindings/export_convolution_ops.cpp
@@ -59,12 +59,13 @@ void export_convolve(){
 template <class M>
 void export_super_to_max(){
 	typedef typename switch_value_type<M,int>::type Mint;
-	def("super_to_max",(void (*)(M&,M&, int, int, Mint*))super_to_max<typename M::value_type, typename M::memory_layout, typename M::index_type>, (
+	def("super_to_max",(void (*)(M&,M&, int, int, Mint*,M*))super_to_max<typename M::value_type, typename M::memory_layout, typename M::index_type>, (
 															arg("dst"),
 															arg("img"),
-															arg("poolsize")),
+															arg("poolsize"),
 															arg("overlap"),
-															arg("indices")
+															arg("indices"),
+															arg("filter"))
 														);
 
 
