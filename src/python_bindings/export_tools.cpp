bc37ec7a9241e1ee73c89b6765280d7214249226
gerharda@AIS.INFORMATIK.UNI-BONN.DE <gerharda@AIS.INFORMATIK.UNI-BONN.DE@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-04-28 14:12:55 +0000

    add memory checks
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@3536 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/python_bindings/export_tools.cpp
+++ src/python_bindings/export_tools.cpp
@@ -1,20 +1,20 @@
 //*LB*
 // Copyright (c) 2010, University of Bonn, Institute for Computer Science VI
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 //  * Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
 //  * Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-//  * Neither the name of the University of Bonn 
+//  * Neither the name of the University of Bonn
 //    nor the names of its contributors may be used to endorse or promote
 //    products derived from this software without specific prior written
 //    permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,43 +27,24 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //*LE*
 
-
-
-
-
 #include <string>
 #include <boost/python.hpp>
 #include <boost/python/extract.hpp>
+#include <pyublas/numpy.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+#include <vector.hpp>
+#include <vector_ops/vector_ops.hpp>
+#include <convert.hpp>
+#include <convolution_ops/convolution_ops.hpp>
+#include <tools/memory_tools.h>
 
 
-#include <cuv_general.hpp>
-#include <random/random.hpp>
-
+//using namespace std;
 using namespace boost::python;
 using namespace cuv;
+namespace ublas = boost::numeric::ublas;
 
-void export_vector();
-void export_vector_ops();
-void export_dense_matrix();
-void export_matrix_ops();
-void export_random();
-void export_dia_matrix();
-void export_convolution_ops();
-void export_image_ops();
-void export_tools();
-
-BOOST_PYTHON_MODULE(cuv_python){
-	def("initCUDA", initCUDA);
-	def("exitCUDA", exitCUDA);
-	def("safeThreadSync", safeThreadSync);
-	def("initialize_mersenne_twister_seeds", initialize_mersenne_twister_seeds);
-	export_vector();
-	export_vector_ops();
-	export_dense_matrix();
-	export_matrix_ops();
-	export_random();
-	export_dia_matrix();
-	export_convolution_ops();
-	export_image_ops();
-	export_tools();
+void export_tools(){
+	def("getFreeMem",(int (*)(int))getFreeDeviceMemory,(arg("dev_idx")=0));
+	def("getMaxMem",(int (*)(int))getMaxDeviceMemory,(arg("dev_idx")=0));
 }
