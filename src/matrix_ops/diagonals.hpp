2bc432e99c19b9e2a7e4b8d11024205bbc67ff9d
Hannes Schulz <schulz@ais.uni-bonn.de>
2010-07-15 11:09:28 +0200

    add support for dealing with diagonals of a DIA matrix

---
+++ src/matrix_ops/diagonals.hpp
@@ -0,0 +1,14 @@
+#include <basics/dia_matrix.hpp>
+
+namespace cuv{
+	
+	/**
+	 * get the average of all diagonals of a diagonal matrix
+	 * 
+	 * @param dst the vector where the results are stored in
+	 * @param dia the diagonal matrix where the diagonals are supposed to be summed
+	 */
+	template<class T, class M, class I>
+	void avg_diagonals( cuv::vector<T,M,I>& dst, const cuv::dia_matrix<T,M>& dia );
+
+}
