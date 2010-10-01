3f82211339278cca87f2364755df8c5729607678
schulz <schulz@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-10-01 10:45:02 +0000

    oups
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4088 9a9a52a9-7681-4bea-9213-143ca318e872

---
+++ src/python_bindings/export_libs_rbm.cpp
@@ -0,0 +1,59 @@
+//*LB*
+// Copyright (c) 2010, University of Bonn, Institute for Computer Science VI
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//  * Neither the name of the University of Bonn 
+//    nor the names of its contributors may be used to endorse or promote
+//    products derived from this software without specific prior written
+//    permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//*LE*
+
+
+
+
+
+#include <string>
+#include <boost/python.hpp>
+#include <boost/python/extract.hpp>
+#include <pyublas/numpy.hpp>
+
+#include <dense_matrix.hpp>
+#include <libs/rbm/rbm.hpp>
+
+//using namespace std;
+using namespace boost::python;
+using namespace cuv;
+using namespace cuv::libs::rbm;
+namespace ublas = boost::numeric::ublas;
+
+template<class V, class L, class M, class I>
+void export_libs_rbm_detail(){
+	typedef dense_matrix<V,L,M,I> mat;
+	typedef vector<V,M,I> vec;
+	def("set_binary_sequence", set_binary_sequence<mat>, (arg("matrix"), arg("startvalue")));
+	def("sigm_temperature", sigm_temperature<mat,vec>, (arg("matrix"), arg("temperature")));
+}
+
+void export_libs_rbm(){
+	export_libs_rbm_detail<float,column_major,host_memory_space,unsigned int>();
+	export_libs_rbm_detail<float,column_major,dev_memory_space,unsigned int>();
+}
