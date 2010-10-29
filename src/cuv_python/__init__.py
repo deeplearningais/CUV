bc804f4eea15951c1b9e0cb592ebb514a094cafe
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-10-29 16:43:12 +0000

    CUV python bindings: removed cp. in init file
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4263 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/cuv_python/__init__.py
+++ src/cuv_python/__init__.py
@@ -3,11 +3,11 @@ from _cuv_python import *
 
 def __cpy(x):
     x2 = x.__class__(x.h,x.w)
-    apply_binary_functor(x2.vec,x.vec,cp.binary_functor.COPY)
+    apply_binary_functor(x2.vec,x.vec,binary_functor.COPY)
     return x2
 
 def __sav_dense(x, file):
-    np.save(file.replace(".npy",""),cp.pull(x))
+    np.save(file.replace(".npy",""),pull(x))
 
 def __shape(x):
     return (x.h,x.w)
@@ -27,8 +27,8 @@ for memory_space in ["dev","host"]:
             dense_type.copy = __cpy
             dense_type.shape = property(__shape)
             dense_type.np = property(__np)
-            dense_type.has_nan = property(lambda x:cp.has_nan(x))
-            dense_type.has_inf = property(lambda x:cp.has_inf(x))
+            dense_type.has_nan = property(lambda x:has_nan(x))
+            dense_type.has_inf = property(lambda x:has_inf(x))
 
     dia_type=eval(memory_space+"_dia_matrix_f")
 
