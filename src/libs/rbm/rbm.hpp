229fc753bd9e68f07acc4f5e6969374f1ebdc5b1
schulz <schulz@9a9a52a9-7681-4bea-9213-143ca318e872>
2010-11-10 15:31:18 +0000

    document lib_rbm functions
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4308 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/libs/rbm/rbm.hpp
+++ src/libs/rbm/rbm.hpp
@@ -5,8 +5,21 @@
 namespace cuv{
 namespace libs{
 namespace rbm{
+	/**
+	 * set a matrix to consecutive binary numbers in the columns, starting with the number `start'
+	 *
+	 * @param m      the target matrix
+	 * @param start  the value of the first column
+	 */
 	template<class __matrix_type>
 	void set_binary_sequence(__matrix_type& m, const int& start);
+
+	/**
+	 * apply sigmoid column-wise with the temperature specified for each column
+	 *
+	 * @param m    source and target matrix
+	 * @param temp the temperature (one value per column)
+	 */
 	template<class __matrix_type,class __vector_type>
 	void sigm_temperature(__matrix_type& m, const __vector_type& temp);
 }
