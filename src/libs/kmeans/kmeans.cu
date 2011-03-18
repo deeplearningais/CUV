3d092f1baab3671d749332416da95fd41f4bf82f
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-03-18 11:27:48 +0000

    host kmeans working
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4702 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/libs/kmeans/kmeans.cu
+++ src/libs/kmeans/kmeans.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../../basics/dense_matrix.hpp"
+#include "../../tools/meta_programming.hpp"
 #include "kmeans.hpp"
 
 using cuv::column_major;
@@ -8,27 +9,104 @@ using cuv::host_memory_space;
 using cuv::dense_matrix;
 using cuv::vector;
 
-template<class V1, class V2>
-__global__
-void compute_clusters_kernel(V1* clusters, const int clusters_h, const int clusters_w, const V1* data, const int data_w, const int data_h, const V2* indices){
-	// threadIdx.x is entry in data vector
-	// blockIdx.x 
-	__shared__ int cluster_index;
+/*template<class V1, class V2>*/
+/*__global__*/
+/*void compute_clusters_kernel(V1* clusters, const int clusters_h, const int clusters_w, const V1* data, const int data_w, const int data_h, const V2* indices){*/
+	/*// threadIdx.x is entry in data vector*/
+	/*// blockIdx.x */
+	/*__shared__ int cluster_index;*/
 
+/*}*/
+
+template<int BLOCK_SIZE, class T, class V>
+__global__
+void compute_clusters_kernel(const T* matrix, T* vector, V* indices, const unsigned int nCols, const unsigned int nRows,
+		const unsigned int num_clusters) {
+
+	typedef typename cuv::unconst<T>::type unconst_value_type;
+
+	extern __shared__ unsigned char ptr[]; // need this intermediate variable for nvcc :-(
+	unconst_value_type* values = (unconst_value_type*) ptr;
+	const unsigned int tx = threadIdx.x;
+	const unsigned int bx = blockIdx.x;
+	const unsigned int ty = threadIdx.y;
+	const unsigned int by = blockIdx.y;
+
+	const int row_idx = by * gridDim.x * blockDim.x +   	// offset according to y index in grid
+						bx * blockDim.x +  					// offset according to block index
+						tx;									// offset in block
+
+	if (row_idx >= nRows)
+		return;
+	const unsigned int off = blockDim.y;
+
+	unconst_value_type sum = 0;
+
+	for (unsigned int my = ty; my < nCols; my += off) {
+		T f = matrix[my * nRows + row_idx ];
+		/*rf.rv(sum,arg_index,f,my);*/
+		//sum=rf(sum,f);
+	}
+
+	values[ty*BLOCK_SIZE+tx] = sum;
+	/*if(functor_traits::returns_index)*/
+		/*indices[ty*BLOCK_SIZE+tx] = arg_index;*/
+
+	__syncthreads();
+
+	for (unsigned int offset = blockDim.y / 2; offset > 0; offset >>=1) {
+		if (ty < offset) {
+			const unsigned int v = ty+offset;
+			/*rf.rr(*/
+					  /*values [ty*BLOCK_SIZE+tx],*/
+					  /*indices[ty*BLOCK_SIZE+tx],*/
+					  /*values [v *BLOCK_SIZE+tx],*/
+					  /*indices[v *BLOCK_SIZE+tx]);*/
+		}
+		__syncthreads();
+	}
+	
+	if (ty == 0) {
+		/*if (functor_traits::returns_index)*/
+			/*vector[row_idx] = indices[tx];*/
+		/*else*/
+				vector[row_idx] = values[tx];
+	}
 }
 
 template<class V, class I>
-void compute_clusters_impl(dense_matrix<V,column_major,dev_memory_space,I>& clusters,
-		const dense_matrix<V,column_major,dev_memory_space,I>& data,
+void compute_clusters_impl(dense_matrix<V,column_major,dev_memory_space,I>& v,
+		const dense_matrix<V,column_major,dev_memory_space,I>& m,
 		const cuv::vector<I,dev_memory_space,I>& indices){
 
-	/*dim3 blocks(,,);*/
-	static const int bs=16*16;
-	int threads = min(bs,data.h());
-	int blocks = data.w();
-	std::cout << "dev implementation" <<std::endl;
-	compute_clusters_kernel<<<blocks,threads>>>(clusters.ptr(), clusters.h(), clusters.w(), data.ptr(), data.w(), data.h(),indices.ptr());
-}
+		cuvAssert(m.ptr() != NULL);
+		cuvAssert(m.h() == v.size());
+		static const int BLOCK_SIZE = 16;
+		static const int BLOCK_DIM_X = BLOCK_SIZE;
+		static const int BLOCK_DIM_Y = BLOCK_SIZE;
+		const int blocks_needed = ceil((float)m.h()/(BLOCK_DIM_X));
+		int grid_x =0, grid_y=0;
+
+		// how to handle grid dimension constraint
+		if (blocks_needed <= 65535){
+			grid_x = blocks_needed;
+			grid_y = 1;
+		}else{
+			// try to avoid large noop blocks by adjusting x and y dimension to nearly equal size
+			grid_x = ceil(sqrt(blocks_needed));
+			grid_y = ceil((float)blocks_needed/grid_x);
+		}
+		dim3 grid(grid_x, grid_y);
+		dim3 threads(BLOCK_DIM_X,BLOCK_DIM_Y);
+
+		const int num_clusters=v.w();
+		std::cout <<" num clusters: " << num_clusters << std::endl;
+		unsigned int mem = sizeof(V) * BLOCK_DIM_X*BLOCK_DIM_Y *num_clusters;
+
+		compute_clusters_kernel<BLOCK_SIZE,V,I><<<grid,threads,mem>>>(m.ptr(),v.ptr(),indices.ptr(), m.w(),m.h(), num_clusters);
+		cuvSafeCall(cudaThreadSynchronize());
+	}
+
 
 template<class V, class I>
 void compute_clusters_impl(dense_matrix<V,column_major,host_memory_space,I>& clusters,
