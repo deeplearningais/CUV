9a7739735c66030a3297d3e3d55805baf023e40d
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-03-28 16:01:23 +0000

    export libs kernel
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4712 9a9a52a9-7681-4bea-9213-143ca318e872

---
+++ src/cuv_python/export_libs_kernels.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <boost/python.hpp>
+#include <boost/python/extract.hpp>
+#include <pyublas/numpy.hpp>
+
+#include <dense_matrix.hpp>
+#include <libs/kernels/kernels.hpp>
+
+//using namespace std;
+using namespace boost::python;
+using namespace cuv;
+using namespace cuv::libs::kernels;
+namespace ublas = boost::numeric::ublas;
+
+template<class V, class L, class M, class I>
+void export_kernels(){
+	typedef dense_matrix<V,L,M,I> mat;
+	def("pairwise_distance",pairwise_distance<mat>,(arg("distances"),arg("X"),arg("Y")));
+}
+
+void export_libs_kernels(){
+	//export_kernels<float,column_major,host_memory_space,unsigned int>();
+	export_kernels<float,row_major,dev_memory_space,unsigned int>();
+}
