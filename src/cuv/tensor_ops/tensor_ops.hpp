3c18a88076904bf38ac6e7768ee088b51f553086
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-04-05 09:09:19 +0000

    made definition of fill more general. let fill value be arbitrary and explicitly convert to value_type.
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4753 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/cuv/tensor_ops/tensor_ops.hpp
+++ src/cuv/tensor_ops/tensor_ops.hpp
@@ -220,9 +220,9 @@ namespace cuv{
    * 
    * This is a convenience wrapper that applies the nullary functor NF_FILL to v.
    */
-  template<class __value_type, class __memory_space_type, class __memory_layout_type>
-  void fill(tensor<__value_type, __memory_space_type, __memory_layout_type>& v, const typename tensor<__value_type, __memory_space_type, __memory_layout_type>::value_type& p){
-      apply_0ary_functor(v,NF_FILL,p);
+  template<class __value_type, class __memory_space_type, class __memory_layout_type, class S>
+  void fill(tensor<__value_type, __memory_space_type, __memory_layout_type>& v, const S& p){
+      apply_0ary_functor(v,NF_FILL,(__value_type)p);
   }
 
 
