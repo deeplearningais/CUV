d1fd811e9107828dbe5edb45f7b48b9af999c56b
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-04-09 10:52:43 +0000

    starting to rework kmeans example so it works with new cuv
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4834 9a9a52a9-7681-4bea-9213-143ca318e872

--- examples/kmeans.py
+++ examples/kmeans.py
@@ -6,26 +6,26 @@ import os
 def kmeans(dataset,num_clusters,iters):
     rand_indices=np.random.randint(0,dataset.shape[1],num_clusters)
     clusters=dataset[:,rand_indices]
-    dataset_dev=cp.push(dataset)
-    clusters_dev=cp.push(clusters.copy("F")) # copy('F') is necessary so we can slice later on
+    dataset_dev=cp.dev_tensor_float(dataset)
+    clusters_dev=cp.dev_tensor_float(clusters.copy("F")) # copy('F') is necessary so we can slice later on
 
-    norms = cp.dev_matrix_cmf(dataset_dev.w, 1)
-    cp.reduce_to_row(norms.vec,dataset_dev,cp.reduce_functor.ADD_SQUARED)
+    norms = cp.dev_tensor_float(dataset_dev.shape[1])
+    cp.reduce_to_row(norms,dataset_dev,cp.reduce_functor.ADD_SQUARED)
 
-    norms_clusters=cp.dev_matrix_cmf(num_clusters,1)
-    dists  = cp.dev_matrix_cmf(dataset_dev.w, num_clusters)
-    nearest= cp.dev_matrix_cmui(dataset_dev.w,1)
-    nearest_dist= cp.dev_matrix_cmf(dataset_dev.w,1)
+    norms_clusters=cp.dev_tensor_float(num_clusters)
+    dists  = cp.dev_tensor_float(dataset_dev.shape[1], num_clusters)
+    nearest= cp.dev_tensor_float_ui(dataset_dev.shape[1])
+    nearest_dist= cp.dev_tensor_float(dataset_dev.shape[1])
 
     for i in xrange(iters):
-        cp.reduce_to_row(norms_clusters.vec,clusters_dev,cp.reduce_functor.ADD_SQUARED)
+        cp.reduce_to_row(norms_clusters,clusters_dev,cp.reduce_functor.ADD_SQUARED)
         cp.prod(dists, dataset_dev, clusters_dev, 't','n',-2, 0)
-        cp.matrix_plus_row(dists,norms_clusters.vec)
-        cp.matrix_plus_col(dists,norms.vec)
-        cp.reduce_to_col(nearest.vec,dists,cp.reduce_functor.ARGMIN)
-        cp.reduce_to_col(nearest_dist.vec,dists,cp.reduce_functor.MIN) # as stopping criterion
+        cp.matrix_plus_row(dists,norms_clusters)
+        cp.matrix_plus_col(dists,norms)
+        cp.reduce_to_col(nearest,dists,cp.reduce_functor.ARGMIN)
+        cp.reduce_to_col(nearest_dist,dists,cp.reduce_functor.MIN) # as stopping criterion
         #print("Average dist to cluster: %f"%cp.mean(nearest_dist))
-        cp.compute_clusters(clusters_dev,dataset_dev,nearest.vec)
+        cp.compute_clusters(clusters_dev,dataset_dev,nearest)
     return [clusters_dev.np, nearest.np]
 
 if __name__ == "__main__":
