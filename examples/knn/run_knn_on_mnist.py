b6325d358e866fc695c42e69c0b83fa5984dc15e
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-04-15 12:08:42 +0000

    remove scipy.io.numpyio from knn example to make it compatible with newer scipy versions
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4905 9a9a52a9-7681-4bea-9213-143ca318e872

--- examples/knn/run_knn_on_mnist.py
+++ examples/knn/run_knn_on_mnist.py
@@ -6,24 +6,23 @@ import numpy as np
 
 class MNISTPatGen:
   def __init__(self,dir):
-      from scipy.io.numpyio import fread
       fd = open(dir+'/train-labels.idx1-ubyte')
-      fread(fd,8,'c')
+      np.fromfile(file=fd, dtype=np.uint8, count=8)
       self.data_labels = np.fromfile(file=fd, dtype=np.uint8).reshape( 60000 )
       fd.close()
 
       fd = open(dir+'/train-images.idx3-ubyte')
-      fread(fd,16,'c')
+      np.fromfile(file=fd, dtype=np.uint8, count=16)
       self.data = np.fromfile(file=fd, dtype=np.uint8).reshape( (60000,784) )
       fd.close()
 
       fd = open(dir+'/t10k-images.idx3-ubyte')
-      fread(fd,16,'c')
+      np.fromfile(file=fd, dtype=np.uint8, count=16)
       self.test = np.fromfile(file=fd, dtype=np.uint8).reshape( (10000,784) )
       fd.close()
 
       fd = open(dir+'/t10k-labels.idx1-ubyte')
-      fread(fd,8,'c')
+      np.fromfile(file=fd, dtype=np.uint8, count=8)
       self.test_labels = np.fromfile(file=fd, dtype=np.uint8).reshape( 10000 )
       fd.close()
   def get_test(self):
