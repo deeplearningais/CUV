1b6658fba4a6d429fca2b774f7eecab8cd1f4cdd
amueller <amueller@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-04-15 17:25:10 +0000

    example from readme
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4917 9a9a52a9-7681-4bea-9213-143ca318e872

---
+++ examples/test1.cpp
@@ -0,0 +1,20 @@
+ #include <cuv/basics/tensor.hpp>
+ #include <cuv/tensor_ops/tensor_ops.hpp>
+ using namespace cuv;
+
+ int main(void){
+ tensor<float,host_memory_space> h(256);  // reserves space in host memory
+ tensor<float,dev_memory_space>  d(256);  // reserves space in device memory
+
+ fill(h,0);                          // terse form
+ apply_0ary_functor(h,NF_FILL,0.f);    // more verbose
+
+ d=h;                                // push to device
+ sequence(d);                        // fill device vector with a sequence
+
+ h=d;                                // pull to host
+ for(int i=0;i<h.size();i++)
+ {
+   assert(d[i] == h[i]);
+ }
+ }
