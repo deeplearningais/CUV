47dae2b1a0c9eb74e324745e6fd729ca0cccdaf7
schulz <schulz@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-05-05 11:19:33 +0000

    make memory-container of tensor a tag-parameter instead of a full-fledged class
    The container-class needs many of the template parameters of the tensor, its specification was therefore redundant.
    
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4946 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/cuv/basics/tensor.hpp
+++ src/cuv/basics/tensor.hpp
@@ -90,16 +90,15 @@ namespace cuv
 	/**
 	 * an n-dimensional tensor with only non-changing accessors
 	 */
-	template<class __value_type, class __memory_space_type, class __memory_layout_type = row_major, class Tptr=const __value_type*,
-	       	class __allocator=linear_memory<__value_type,__memory_space_type,Tptr,unsigned int> >
+	template<class __value_type, class __memory_space_type, class __memory_layout_type = row_major, class Tptr=const __value_type*, class __mem_container=linear_memory_tag>
 	class const_tensor{
 		public:
+			typedef unsigned int index_type;
 			typedef typename unconst<__value_type>::type value_type;	///< Type of the entries of matrix
 			typedef const value_type const_value_type;	///< Type of the entries of matrix
 			typedef __memory_layout_type  memory_layout_type; ///< host or device
 			typedef __memory_space_type memory_space_type; ///< C or Fortran storage
-			typedef __allocator memory_container_type;     ///< the thing that allocates our storage
-			typedef typename memory_container_type::index_type index_type;       ///< the type of the tensor indices
+			typedef typename memory_traits<__value_type,__memory_space_type,Tptr,index_type,__mem_container>::type  memory_container_type;     ///< the thing that allocates our storage
 			typedef typename memory_container_type::reference_type reference_type;       ///< the type of the references returned by access operator
 			typedef typename memory_container_type::const_reference_type const_reference_type;
 			typedef typename memory_container_type::pointer_type pointer_type;  ///< type of stored pointer, could be const or not-const value_type*
@@ -426,12 +425,12 @@ namespace cuv
 	/**
 	 * Provides non-const accessors to const_tensor
 	 */
-	template<class __value_type, class __memory_space_type, class __memory_layout_type=row_major, class __allocator=linear_memory<__value_type,__memory_space_type,__value_type*,unsigned int> >
+	template<class __value_type, class __memory_space_type, class __memory_layout_type=row_major, class __mem_container=linear_memory_tag >
 	class tensor
-	: public const_tensor<__value_type, __memory_space_type,__memory_layout_type, __value_type*, __allocator>
+	: public const_tensor<__value_type, __memory_space_type,__memory_layout_type, __value_type*, __mem_container>
 	{
 		public:
-			typedef const_tensor<__value_type, __memory_space_type, __memory_layout_type, __value_type*,__allocator> super_type;
+			typedef const_tensor<__value_type, __memory_space_type, __memory_layout_type, __value_type*,__mem_container> super_type;
 			typedef typename super_type::value_type                      value_type;
 			typedef typename super_type::const_value_type          const_value_type;
 			typedef typename super_type::memory_space_type        memory_space_type;
