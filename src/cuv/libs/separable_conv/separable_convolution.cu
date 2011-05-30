d5d186cf72759002a92e4d046a6e97a2f8dd15d8
schulz <schulz@9a9a52a9-7681-4bea-9213-143ca318e872>
2011-05-30 12:47:24 +0000

    generalized separable convolutions for interleaved images (currently does not work, since the wrapped kernel uses too many registers even for standard float)
    git-svn-id: https://svn.ais.uni-bonn.de/svn/repos/cuv/trunk@4998 9a9a52a9-7681-4bea-9213-143ca318e872

--- src/cuv/libs/separable_conv/separable_convolution.cu
+++ src/cuv/libs/separable_conv/separable_convolution.cu
@@ -4,6 +4,7 @@
 
 #include <cuv/basics/tensor.hpp>
 #include <cuv/tensor_ops/tensor_ops.hpp>
+#include <cuv/tensor_ops/functors.hpp>
 #include <cuv/libs/separable_conv/separable_convolution.hpp>
 
 namespace cuv{
@@ -24,16 +25,18 @@ namespace cuv{
 #define ROWS_RESULT_STEPS 8
 #define   ROWS_HALO_STEPS 1
 
+		/*template<int KERNEL_RADIUS, class SrcT, class DstT, class BinFuncConv>*/
 		template<int KERNEL_RADIUS, class SrcT, class DstT>
 		__global__ void convolutionRowGPU(
 				SrcT *d_Dst,
-				DstT *d_Src,
+				const DstT *d_Src,
 				int imageW,
 				int imageH,
 				int dpitch,
-				int spitch 
+				int spitch/*,*/
+				/*BinFuncConv mult*/
 				){
-			__shared__ float s_Data[ROWS_BLOCKDIM_Y][(ROWS_RESULT_STEPS + 2 * ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X];
+			__shared__ SrcT s_Data[ROWS_BLOCKDIM_Y][(ROWS_RESULT_STEPS + 2 * ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X];
 
 			//Offset to the left halo edge
 			const int baseX = (blockIdx.x * ROWS_RESULT_STEPS - ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X + threadIdx.x;
@@ -59,13 +62,16 @@ namespace cuv{
 
 			//Compute and store results
 			__syncthreads();
+
 #pragma unroll
 			for(int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++){
 				float sum = 0;
 
 #pragma unroll
-				for(int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++)
-					sum += c_Kernel[KERNEL_RADIUS - j] * s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X + j];
+				for(int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++){
+					/*sum += mult(s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X + j], c_Kernel[KERNEL_RADIUS - j]);*/
+					sum += s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X + j]* c_Kernel[KERNEL_RADIUS - j];
+				}
 
 				d_Dst[i * ROWS_BLOCKDIM_X] = sum;
 			}
@@ -79,14 +85,16 @@ namespace cuv{
 #define COLUMNS_RESULT_STEPS 8
 #define   COLUMNS_HALO_STEPS 1
 
+		/*template<int KERNEL_RADIUS, class SrcT, class DstT, class BinFuncConv>*/
 		template<int KERNEL_RADIUS, class SrcT, class DstT>
 		__global__ void convolutionColumnGPU(
-				SrcT *d_Dst,
-				DstT *d_Src,
+				DstT *d_Dst,
+				const SrcT *d_Src,
 				int imageW,
 				int imageH,
 				int dpitch,
 				int spitch
+				/*BinFuncConv mult*/
 				){
 			__shared__ float s_Data[COLUMNS_BLOCKDIM_X][(COLUMNS_RESULT_STEPS + 2 * COLUMNS_HALO_STEPS) * COLUMNS_BLOCKDIM_Y + 1];
 
@@ -118,7 +126,7 @@ namespace cuv{
 				float sum = 0;
 #pragma unroll
 				for(int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++)
-					sum += c_Kernel[KERNEL_RADIUS - j] * s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y + j];
+					sum += s_Data[threadIdx.x][threadIdx.y + i * COLUMNS_BLOCKDIM_Y + j]* c_Kernel[KERNEL_RADIUS - j];
 				if(imageH - baseY > i * COLUMNS_BLOCKDIM_Y)
 					*PITCH(d_Dst,dpitch,i*COLUMNS_BLOCKDIM_Y,0) = sum;
 			}
@@ -129,38 +137,64 @@ namespace cuv{
 			return (a % b != 0) ? (a / b + 1) : (a / b);  	  	 
 		} 
 #define V(X) #X << " : "<< (X)<<"  "
-		template<int radius, class DstV, class SrcV, class A>
+		template<int radius, int channels, class DstV, class SrcV, class A>
 		void convolve_call_kernel(tensor<DstV,dev_memory_space,row_major,A>& dst,
-				     const tensor<SrcV,dev_memory_space,row_major,A>& src, int dir){
+				     const tensor<SrcV,dev_memory_space,row_major,A>& src, int dir
+				     ){
 
-			int dw = src.shape()[1];
+			int dw = src.shape()[1]/channels;
 			int dh = src.shape()[0];
-			
+
+			typedef vector_type_traits<DstV> dst_traits;
+			typedef vector_type_traits<SrcV> src_traits;
+			typedef typename dst_traits::template vector<channels>::type* dst_vec_t;
+			typedef typename src_traits::template vector<channels>::type* src_vec_t;
+
 			if(dir==0){
 				dim3 blocks(iDivUp(dw , (ROWS_RESULT_STEPS * ROWS_BLOCKDIM_X)), iDivUp(dh , ROWS_BLOCKDIM_Y));
 				dim3 threads(ROWS_BLOCKDIM_X, ROWS_BLOCKDIM_Y);
-				convolutionRowGPU<radius><<<blocks, threads>>>( dst.ptr(), src.ptr(), src.shape()[1], src.shape()[0],dst.pitch(),src.pitch());
+				 convolutionRowGPU<radius><<<blocks, threads>>>( (dst_vec_t) dst.ptr(), (src_vec_t) src.ptr(), dw, dh,dst.pitch(),src.pitch());
+				/*convolutionRowGPU<radius><<<blocks, threads>>>( (dst_vec_t) dst.ptr(), (src_vec_t) src.ptr(), dw, dh,dst.pitch(),src.pitch(),*/
+						/*make_bf_vd_vd<channels,1>(bf_multiplies<DstV,SrcV,float>()));*/
 			}else if(dir==1){
 				dim3 blocks(iDivUp(dw , COLUMNS_BLOCKDIM_X), iDivUp(dh , (COLUMNS_RESULT_STEPS * COLUMNS_BLOCKDIM_Y)));
 				dim3 threads(COLUMNS_BLOCKDIM_X, COLUMNS_BLOCKDIM_Y);
-				convolutionColumnGPU<radius><<<blocks, threads>>>( dst.ptr(), src.ptr(), src.shape()[1], src.shape()[0], dst.pitch(), src.pitch());
+				convolutionColumnGPU<radius><<<blocks, threads>>>( (dst_vec_t) dst.ptr(), (src_vec_t) src.ptr(), dh, dw, dst.pitch(), src.pitch());
+				/*convolutionColumnGPU<radius><<<blocks, threads>>>( (dst_vec_t) dst.ptr(), (src_vec_t) src.ptr(), dh, dw, dst.pitch(), src.pitch(),*/
+						/*make_bf_vd_vd<channels,1>(bf_multiplies<DstV,SrcV,float>()));*/
 			}
 			cuvSafeCall(cudaThreadSynchronize());
 		}
 
 
+		template<int Channels,class DstV, class SrcV, class M, class A>
+		void radius_dispatch(const unsigned int& radius,
+					   interleaved_image<Channels,DstV,M>& dst,
+				     const interleaved_image<Channels,SrcV,M>& src,int dir){
+			switch(radius){
+				case 1: convolve_call_kernel<1,Channels>(dst.tens(),src.tens(),dir); break;
+				case 2: convolve_call_kernel<2,Channels>(dst.tens(),src.tens(),dir); break;
+				case 3: convolve_call_kernel<3,Channels>(dst.tens(),src.tens(),dir); break;
+				case 4: convolve_call_kernel<4,Channels>(dst.tens(),src.tens(),dir); break;
+				case 5: convolve_call_kernel<5,Channels>(dst.tens(),src.tens(),dir); break;
+				case 6: convolve_call_kernel<6,Channels>(dst.tens(),src.tens(),dir); break;
+				case 7: convolve_call_kernel<7,Channels>(dst.tens(),src.tens(),dir); break;
+				case 8: convolve_call_kernel<8,Channels>(dst.tens(),src.tens(),dir); break;
+				default: cuvAssert(false);
+			}
+		}
 		template<class DstV, class SrcV, class M, class A>
 		void radius_dispatch(const unsigned int& radius,tensor<DstV,M,row_major,A>& dst,
 				     const tensor<SrcV,M,row_major,A>& src,int dir){
 			switch(radius){
-				case 1: convolve_call_kernel<1>(dst,src,dir); break;
-				case 2: convolve_call_kernel<2>(dst,src,dir); break;
-				case 3: convolve_call_kernel<3>(dst,src,dir); break;
-				case 4: convolve_call_kernel<4>(dst,src,dir); break;
-				case 5: convolve_call_kernel<5>(dst,src,dir); break;
-				case 6: convolve_call_kernel<6>(dst,src,dir); break;
-				case 7: convolve_call_kernel<7>(dst,src,dir); break;
-				case 8: convolve_call_kernel<8>(dst,src,dir); break;
+				case 1: convolve_call_kernel<1,1>(dst,src,dir); break;
+				case 2: convolve_call_kernel<2,1>(dst,src,dir); break;
+				case 3: convolve_call_kernel<3,1>(dst,src,dir); break;
+				case 4: convolve_call_kernel<4,1>(dst,src,dir); break;
+				case 5: convolve_call_kernel<5,1>(dst,src,dir); break;
+				case 6: convolve_call_kernel<6,1>(dst,src,dir); break;
+				case 7: convolve_call_kernel<7,1>(dst,src,dir); break;
+				case 8: convolve_call_kernel<8,1>(dst,src,dir); break;
 				default: cuvAssert(false);
 			}
 		}
@@ -222,6 +256,39 @@ namespace cuv{
 				radius_dispatch(filter_radius,dst,tmp,1);
 			}
 		}
+
+		template<int Channels,class DstV, class SrcV, class M>
+		void
+		convolve(       interleaved_image<Channels,DstV,M>& dst,
+			  const interleaved_image<Channels,SrcV,M>& src,
+			  const unsigned int&   filter_radius,
+			  const separable_filter& filt, int axis, 
+			  const float& param ){
+
+			typedef interleaved_image<Channels,DstV,M> result_type;
+			typedef interleaved_image<Channels,SrcV,M>    src_type;
+			cuvAssert(filter_radius <= MAX_KERNEL_RADIUS);
+
+			if(filt == SP_GAUSS){
+				const int kernel_w = 2*filter_radius+1;
+				cuv::tensor<float, host_memory_space> kernel(kernel_w);
+				for(int i = 0; i < kernel_w; i++){
+					float dist = (float)(i - (int)filter_radius);
+					kernel[i]  = expf(- dist * dist / (2*param*param));
+				}
+				kernel /= cuv::sum(kernel);
+				cuvSafeCall( cudaMemcpyToSymbol(c_Kernel, kernel.ptr(), kernel.memsize()) );
+				result_type tmp(src.height(), src.width(), src.channels());
+
+				float4 f4;
+				float f;
+				f4 = make_uf_vd_vd<4>(uf_abs<float,float>())(f4);
+				f4 = make_bf_vd_vd<4,1>(bf_plus<float,float,float>())(f4,f);
+				f4 = make_bf_vd_vd<4,4>(bf_plus<float,float,float>())(f4,f4);
+				/*radius_dispatch(filter_radius,tmp,src,0);*/
+				/*radius_dispatch(filter_radius,dst,tmp,1);*/
+			}	
+		}
 		
 		// instantiations
 #define INST(DSTV, SRCV,M,A) \
@@ -231,7 +298,16 @@ namespace cuv{
 				const unsigned int&,                     \
 				const separable_filter&, int axis, \
 				const float&);
+#define INST_IL(CHANNELS,DSTV, SRCV,M) \
+		template void \
+		convolve<CHANNELS,DSTV,SRCV,M>( interleaved_image<CHANNELS,DSTV,M>&, \
+				const interleaved_image<CHANNELS,SRCV,M>&, \
+				const unsigned int&,                     \
+				const separable_filter&, int axis, \
+				const float&);
 		INST(float,float,dev_memory_space,linear_memory_tag);
 		INST(float,float,dev_memory_space,memory2d_tag);
+
+		INST_IL(4,float,float,dev_memory_space);
 	} // namespace separable convolution
 } // namespace cuv
--- src/cuv/libs/separable_conv/separable_convolution.hpp
+++ src/cuv/libs/separable_conv/separable_convolution.hpp
@@ -3,6 +3,7 @@
 
 //#include <boost/ptr_container/ptr_vector.hpp>
 #include <cuv/basics/tensor.hpp>
+#include <cuv/basics/image.hpp>
 
 namespace cuv{
 	namespace sep_conv
@@ -11,11 +12,21 @@ namespace cuv{
 			SP_GAUSS,
 			SP_CENTERED_DERIVATIVE,
 			SP_BOX,
+			SP_ORIENTATION_AND_MAGNITUDE,
 		};
 		template<class DstV, class SrcV, class M, class A>
 		void
 		convolve(  tensor<DstV,M,row_major, A>& dst,
 			   const tensor<SrcV,M,row_major, A>& src,
+			   const unsigned int&   radius,
+			   const separable_filter& filt, int axis=2,
+			   const float& param=0.
+		       	);
+
+		template<int Channels, class DstV, class SrcV, class M>
+		void
+		convolve(        interleaved_image<Channels,DstV,M>& dst,
+			   const interleaved_image<Channels,SrcV,M>& src,
 			   const unsigned int&   radius,
 			   const separable_filter& filt, int axis=2,
 			   const float& param=0.
