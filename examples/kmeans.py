efa62467289356ea3a59a007fa17c6d1e17d8c1c
Andy <amueller@ais.uni-bonn.de>
2011-07-06 14:23:36 +0200

    kmeans example: make everything row major. less transposing and stuff... also some new comments

--- examples/kmeans.py
+++ examples/kmeans.py
@@ -3,21 +3,26 @@ import numpy as np
 import os
 
 def kmeans(dataset,num_clusters,iters):
-    rand_indices=np.random.randint(0,dataset.shape[1],num_clusters)
-    clusters=dataset[:,rand_indices]
-    dataset_dev=cp.dev_tensor_float_cm(dataset)
-    clusters_dev=cp.dev_tensor_float_cm(clusters.copy("F")) # copy('F') is necessary so we can slice later on
+    # initialize clusters randomly
+    rand_indices=np.random.randint(0,dataset.shape[0],num_clusters)
+    clusters=dataset[rand_indices,:]
 
-    norms = cp.dev_tensor_float(dataset_dev.shape[1])
-    cp.reduce_to_row(norms,dataset_dev,cp.reduce_functor.ADD_SQUARED)
+    # push initial clusters and dataset to device
+    dataset_dev=cp.dev_tensor_float(dataset)
+    clusters_dev=cp.dev_tensor_float(clusters)
 
-    dists = cp.dev_tensor_float([dataset_dev.shape[1], num_clusters])
-    nearest = cp.dev_tensor_uint(dataset_dev.shape[1])
+    # allocate matrices for calculations (so we don't need to allocate in loop)
+    dists = cp.dev_tensor_float([dataset_dev.shape[0], num_clusters])
+    nearest = cp.dev_tensor_uint(dataset_dev.shape[0])
 
+    # main loop
     for i in xrange(iters):
-        cp.pdist2(dists, dataset_dev.T,clusters_dev.T)
-        cp.reduce_to_col(nearest,dists,cp.reduce_functor.ARGMIN)
-        cp.compute_clusters(clusters_dev,dataset_dev,nearest)
+        # compute pairwise distances
+        cp.pdist2(dists, dataset_dev, clusters_dev)
+        # find closest cluster
+        cp.reduce_to_col(nearest, dists, cp.reduce_functor.ARGMIN)
+        # update cluster centers (this is a special purpose function for kmeans)
+        cp.compute_clusters(clusters_dev, dataset_dev, nearest)
     return [clusters_dev.np, nearest.np]
 
 if __name__ == "__main__":
@@ -26,13 +31,13 @@ if __name__ == "__main__":
 
     with open(os.path.join(path,'train-images.idx3-ubyte')) as fd:
         np.fromfile(file=fd,dtype=np.uint8,count=16)
-        mnist = np.fromfile(file=fd, dtype=np.uint8).reshape( (60000,784)).astype('float32').T
+        mnist = np.fromfile(file=fd, dtype=np.uint8).reshape( (60000,784)).astype('float32')
 
     num_clusters=20
 
-    [clusters, indices]=kmeans(mnist,num_clusters,10)
+    [clusters, indices]=kmeans(mnist, num_clusters,10)
 
     for i in xrange(num_clusters):
         plt.subplot(int(np.ceil(num_clusters/5)),5,i+1)
-        plt.imshow(clusters[:,i].reshape(28,28))
+        plt.imshow(clusters[i].reshape(28,28))
     plt.show()
