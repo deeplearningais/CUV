35e39bbfcc17530ca956f51fed86cffcf37414b8
Andy <amueller@ais.uni-bonn.de>
2011-08-17 11:34:51 +0200

    some documentation of device tools

--- src/cuv/tools/device_tools.h
+++ src/cuv/tools/device_tools.h
@@ -21,9 +21,38 @@
 #include "exception_helper.hpp"
 
 namespace cuv{
+        /** 
+         * @brief Get available memory on device
+         * 
+         * @param dev_idx Device id. Tries to set cuda context to this device.
+         * 
+         * @return Bytes of free memory.
+         *
+         * This function is highly discouraged and deprecated. It tries to fill the memory.
+         * It should be replace by a call to the cuda API.
+         */
 	int getFreeDeviceMemory(int dev_idx=0);
+
+        /** 
+         * @brief Get device memory
+         * 
+         * @param dev_idx Device id.
+         * 
+         * @return Memory of GPU in bytes.
+         */
 	int getMaxDeviceMemory(int dev_idx=0);
+
+        /** 
+         * @brief Returns number of CUDA devices
+         * 
+         * @return Number of CUDA devices
+         */
 	int countDevices();
+        /** 
+         * @brief Get device id of current CUDA context
+         * 
+         * @return device id of current device
+         */
         int getCurrentDevice();
 }
 #endif /* MEMORY_TOOLS_H_ */
