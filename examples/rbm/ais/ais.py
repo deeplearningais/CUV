d4a7cad2e18cc93dfa67303f50fd10e005061f17
Andy <amueller@ais.uni-bonn.de>
2011-09-02 14:45:58 +0200

    remove unused functions, cleanup

--- examples/rbm/ais/ais.py
+++ examples/rbm/ais/ais.py
@@ -1,24 +1,37 @@
-import warnings
-warnings.filterwarnings("ignore")
 import time
 import sys
+
+from time import strftime
+import os
+import numpy as np
+import cPickle
+
 sys.path.append(".")
 sys.path.append("..")
 
-import time
-from time import strftime
-
-import os
 import cuv_python as cp
-from sensibleconfig import *
-from helper_classes import *
-from datasets import *
-from ais_functions import *
+from sensibleconfig import Config, Option
+from helper_classes import Dataset, UnitType
+from datasets import MNISTData, ShifterData, BarsAndStripesData
+
+
+def sigm(x):
+    return 1/(1+np.exp(-x))
+
+def sample(x,unit_type):
+
+    if unit_type=="gaussian":
+        y = (x + np.random.normal(0,1,x.shape)).astype('float32')
+    else:
+        noise = (np.random.uniform(0,1,x.shape)).astype('float32')
+        y = (noise < x).astype("float32")
+    return y
 
 def rect(x):
     i=x<87
     x[i]=np.log(1+np.exp(x[i]))
     return x
+
 
 class ais(object):
     def __init__(self,cfg,dataset) :
@@ -32,28 +45,28 @@ class ais(object):
     def initialize_everything(self):
         ### initialize matrices for cuda ###
         self.r_ =np.zeros(self.cfg.chains).astype('float32').copy('F')
-        self.w = cp.push(self.w_.copy("F"))
+        self.w = cp.dev_tensor_float_cm(self.w_.copy("F"))
 
 
         ### generate basemodel
         softened = (self.data.mean(axis=1) + 0.1)
         self.baserate_bias_= (np.log(softened) - np.log(1-softened)).astype('float32').copy('F')
-        self.baserate_bias_.shape=(self.w.h,1)
+        self.baserate_bias_.shape=(self.w.shape[0],1)
 
 
         ## start chains
         self.v_ = np.tile(sigm(self.baserate_bias_),(1,self.cfg.chains))
         self.v_ = sample(self.v_,self.cfg['utype']).astype('float32').copy('F')
-        self.v = cp.push(self.v_.copy("F"))
-        self.h = cp.dev_matrix_cmf(self.num_hids,self.cfg.chains)
+        self.v = cp.dev_tensor_float_cm(self.v_.copy("F"))
+        self.h = cp.dev_tensor_float_cm([self.num_hids,self.cfg.chains])
 
-        self.baserate_bias = cp.push(self.baserate_bias_.copy("F"))
-        self.r = cp.push(np.vstack(self.r_).copy("F"))
+        self.baserate_bias = cp.dev_tensor_float_cm(self.baserate_bias_.copy("F"))
+        self.r = cp.dev_tensor_float_cm(np.vstack(self.r_).copy("F"))
         cp.initialize_mersenne_twister_seeds(int(time.time()*1000) % 100000)
 
     def get_partition_function(self):
-        tmp = cp.dev_matrix_cmf(self.cfg.chains,1)
-        tmp2 = cp.dev_matrix_cmf(self.num_hids,self.cfg.chains)
+        tmp = cp.dev_tensor_float_cm([self.cfg.chains, 1])
+        tmp2 = cp.dev_tensor_float_cm([self.num_hids,self.cfg.chains])
         #steps = 14500
         #steps = 1000
         steps = self.cfg.steps
@@ -75,9 +88,9 @@ class ais(object):
                 #beta += 0.00001
             beta += 1.0/steps
             #if step % 100 == 0:
-                #self.r_=cp.pull(self.r)
-                #v_=cp.pull(self.v)
-                #h_=cp.pull(self.h)
+                #self.r_=self.r.np
+                #v_=self.v.np
+                #h_=self.h.np
                 #print "v: %f"%v_.mean()
                 #print "h: %f"%h_.mean()
                 #print "r: %f"%self.r_.mean()
@@ -85,7 +98,7 @@ class ais(object):
                 #sys.stdout.flush()
 
         ### multiply r by partition function of baseline rbm
-        self.r_=cp.pull(self.r)
+        self.r_=self.r.np
         self.partition_baserate = (np.log(1+np.exp(self.baserate_bias_))).sum()+self.num_hids*np.log(2)
         self.r_ += self.partition_baserate
         tmp.dealloc()
@@ -113,33 +126,32 @@ class ais(object):
             self.load_weights(self.cfg.path)
             self.initialize_everything()
 
-            Z = self.get_partition_function()
+            self.get_partition_function()
             Z_dev=self.r_.std()
-            Z_mean = self.r_.mean() 
+            Z_mean = self.r_.mean()
             print "\npartition function: " + str(Z_mean),
             print "standarddeviation: " + str(Z_dev)
-            
             p_v = self.get_data_likelihood()
             print "dataloglikelihood  mean: " + str((p_v).mean())+ " std-deviation: " + str(p_v.std())
+
             end_time = time.clock()
             stats['ais_z_%d'%steps]=Z_mean
             stats['ais_p_nats_%d'%steps]=p_v.mean()
             stats['ais_z_std_%d'%steps]=Z_dev
 
-            stats['ais_dev'     ]     = self.cfg.device
-            stats['basename']     = self.cfg.path
-            stats['bindex'  ]     = self.cfg.postfix
-            stats['ais_host']     = os.uname()[1]
-            stats['ais_time'    ] = strftime("%Y-%m-%d %H:%M:%S")
+            stats['ais_dev'] = self.cfg.device
+            stats['basename'] = self.cfg.path
+            stats['bindex'] = self.cfg.postfix
+            stats['ais_host'] = os.uname()[1]
+            stats['ais_time'] = strftime("%Y-%m-%d %H:%M:%S")
             stats['ais_duration'] = end_time - start_time
 
             with open(fn,"wb") as f:
                 cPickle.dump(stats,f)
-        
 
     def get_data_likelihood(self):
         ### calculate data loglikelihood
-        p_v= (rect(np.dot(self.w_.T,self.v_org)+cp.pull(self.bias_hi)).sum(axis=0) +np.dot(cp.pull(self.bias_lo).T,self.v_org))-self.r_.mean()
+        p_v= (rect(np.dot(self.w_.T,self.v_org)+self.bias_hi.np).sum(axis=0) +np.dot(self.bias_lo.np.T,self.v_org))-self.r_.mean()
         print "datalikelihood under baserate model: ", (np.dot(self.baserate_bias_.T,self.v_org) - self.partition_baserate).mean()
         return p_v
 
@@ -149,50 +161,48 @@ class ais(object):
             print "Could not open weights."
             sys.exit(1)
         self.w_ =np.load(os.path.join(path,"weights-0-%s.npy"%self.cfg['postfix']))
-        self.bias_lo = cp.push(np.load(os.path.join(path,"bias-lo-0-%s.npy"%self.cfg.postfix)))
-        self.bias_hi = cp.push(np.load(os.path.join(path,"bias-hi-0-%s.npy"%self.cfg.postfix)))
-        self.w=cp.push(self.w_.copy("F"))
+        self.bias_lo = cp.dev_tensor_float((np.load(os.path.join(path,"bias-lo-0-%s.npy"%self.cfg.postfix))).reshape(-1,1))
+        self.bias_hi = cp.dev_tensor_float((np.load(os.path.join(path,"bias-hi-0-%s.npy"%self.cfg.postfix))).reshape(-1,1))
+        self.w=cp.dev_tensor_float_cm(self.w_.copy("F"))
         self.num_vis=self.w_.shape[0]
         self.num_hids=self.w_.shape[1]
         print "Number of hidden units: ",self.num_hids
-
-
 
     def p_k(self,beta,tmp,tmp2,collect):
         cp.prod(tmp,self.v,self.baserate_bias,'t','n')
         cp.apply_scalar_functor(tmp,cp.scalar_functor.MULT,(1-beta))
         collect(tmp)
         cp.prod(tmp2,self.w,self.v,'t','n')
-        cp.matrix_plus_col(tmp2,self.bias_hi.vec)
+        cp.matrix_plus_col(tmp2,self.bias_hi)
 
         cp.apply_scalar_functor(tmp2,cp.scalar_functor.MULT,beta)
 
         # RECT computes log(1+exp(x))
-        cp.apply_scalar_functor(tmp2.vec,cp.scalar_functor.RECT,1)
+        cp.apply_scalar_functor(tmp2,cp.scalar_functor.RECT,1)
 
-        cp.reduce_to_row(tmp.vec,tmp2,cp.reduce_functor.ADD)
-        collect(tmp) 
-        cp.prod(tmp,self.v,self.bias_lo,'t','n')
+        cp.reduce_to_row(tmp.T,tmp2,cp.reduce_functor.ADD) # tmp.T is an evil hack. it makes tmp into row major, which doesn't change anything since it's a vector any way. But vectors are always assumed to be row major.
+        collect(tmp)
+        cp.prod(tmp,self.v,self.bias_lo.T,'t','n')
         cp.apply_scalar_functor(tmp,cp.scalar_functor.MULT,beta)
         collect(tmp)
 
     def sample_markov_chains(self,beta,step):
         cp.prod(self.h,self.w,self.v,'t','n')
-        cp.matrix_plus_col(self.h,self.bias_hi.vec)
+        cp.matrix_plus_col(self.h,self.bias_hi)
         cp.apply_scalar_functor(self.h,cp.scalar_functor.MULT,beta)
         cp.apply_scalar_functor(self.h,cp.scalar_functor.SIGM)
         cp.rnd_binarize(self.h)
         cp.prod(self.v,self.w,self.h,'n','n')
-        cp.matrix_plus_col(self.v,self.bias_lo.vec)
+        cp.matrix_plus_col(self.v,self.bias_lo)
         cp.apply_scalar_functor(self.v,cp.scalar_functor.MULT,beta)
         cp.apply_scalar_functor(self.baserate_bias,cp.scalar_functor.MULT,1-beta)
 
-        cp.matrix_plus_col(self.v,self.baserate_bias.vec)
+        cp.matrix_plus_col(self.v,self.baserate_bias)
         cp.apply_scalar_functor(self.baserate_bias,cp.scalar_functor.MULT,1.0/(1-beta))
         cp.apply_scalar_functor(self.v,cp.scalar_functor.SIGM)
         #if step % 100 == 0:
            #plt.figure(1)
-           #self.v_=cp.pull(self.v)
+           #self.v_=self.v.np
            #showthis = self.v_.copy()
            #plt.matshow(showthis[:,0].reshape((28,28)))
            #plt.draw()
@@ -206,11 +216,11 @@ if __name__=='__main__':
 
     loc = locals()
     options = [
-        Option('device',     'GPU device to use [%default]', 0,                   short_name='d', converter=int),
-        Option('chains',     'number of markov chains [%default]', 512,                 short_name='c', converter=int),
+        Option('device',    'GPU device to use [%default]', 0, short_name='d', converter=int),
+        Option('chains',     'number of markov chains [%default]', 512, short_name='c', converter=int),
         Option('steps', 'number of annealing steps (don\'t change this at the moment [%default]', 145000,  short_name='s', converter=int),
         Option('postfix',     'index/postfix of file [%default]', "pretrain", short_name='i', converter=str),
-        Option('utype',  'type of visible units (cont/gaussian/binary) [%default]', 'binary.binary', short_name='V', converter=lambda x: [eval("pyrbm.UnitType."+y, loc) for y in x.split(".")]),
+        Option('utype',  'type of visible units (cont/gaussian/binary) [%default]', 'binary.binary', short_name='V', converter=lambda x: [eval("UnitType."+y, loc) for y in x.split(".")]),
         Option('path',    'Path to weights [%default]', '.', short_name='p', converter=str),
 ]
 
@@ -219,11 +229,10 @@ if __name__=='__main__':
         sys.path.insert(0,os.path.join(os.getenv('HOME'),'bin'))
         import optcomplete
         optcomplete.autocomplete(cfg._parser)
-    except ImportError: 
+    except ImportError:
         pass
 
-    
-    cfg.grab_from_argv(sys.argv)    
+    cfg.grab_from_argv(sys.argv)
     cfg.batchsize=-1 # not used, only compatibility with dataset classes
 
 
