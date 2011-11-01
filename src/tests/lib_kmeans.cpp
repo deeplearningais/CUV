7779f8a30b1e4b474708c48499f0c9b4747932c1
Hannes Schulz <schulz@ais.uni-bonn.de>
2011-11-01 14:06:49 +0100

    added speed test for sort_by_idx

--- src/tests/lib_kmeans.cpp
+++ src/tests/lib_kmeans.cpp
@@ -31,10 +31,25 @@
 #include <numeric>
 #include <boost/test/included/unit_test.hpp>
 #include <cuv/tools/cuv_test.hpp>
+#include <cuv/tools/timing.hpp>
 #include <cuv/tools/cuv_general.hpp>
 #include <cuv/basics/tensor.hpp>
 #include <cuv/libs/kmeans/kmeans.hpp>
 using namespace cuv;
+
+#define MEASURE_TIME(MSG, OPERATION, ITERS)     \
+	float MSG;                                  \
+	if(1){                                      \
+		Timing tim;                             \
+		for(int i=0;i<ITERS;i++){               \
+			OPERATION ;                         \
+                        safeThreadSync();               \
+		}                                       \
+		tim.update(ITERS);                      \
+		printf("%s [%s] took %4.4f us/pass\n", #MSG, #OPERATION, 1000000.0f*tim.perf()); \
+		MSG = 1000000.0f*tim.perf();            \
+	}
+
 
 struct MyConfig {
 	static const int dev = CUDA_TEST_DEVICE;
@@ -56,11 +71,44 @@ struct Fix{
 };
 
 
+bool
+time_sort_by_idx_impl(unsigned int data_num=16, unsigned int data_len=4){
+	tensor<float,host_memory_space> data(extents[data_num][data_len]);
+	tensor<unsigned int, host_memory_space> indices(data_num);
+
+	std::vector<unsigned int> hidx(data_num);
+	for (unsigned int i = 0; i < data_num; ++i)
+		hidx[i] = i;
+
+	std::random_shuffle(hidx.begin(), hidx.end());
+
+	for (unsigned int i = 0; i < data_num; ++i)
+		indices[i] = hidx[i];
+
+	for (unsigned int i = 0; i < data_num; ++i)
+	{
+		for (unsigned int j = 0; j < data_len; ++j)
+		{
+			data(i,j) = (float) hidx[i];
+		}
+	}
+
+	tensor<float, host_memory_space> sorted(data.shape());
+
+	using cuv::libs::kmeans::sort_by_index;
+
+	tensor<float,dev_memory_space> data_d(data);
+	tensor<float,dev_memory_space> sorted_d(sorted);
+	tensor<unsigned int,dev_memory_space> indices_d(indices);
+
+	MEASURE_TIME(host, sort_by_index(sorted,indices,data), 5);
+	MEASURE_TIME(dev,  sort_by_index(sorted_d,indices_d,data_d), 5);
+	printf("Speedup: %3.4f\n", host/dev);
+}
+
 template<class M>
-void
-test_sort_by_idx_impl(){
-	unsigned int data_num = 16;
-	unsigned int data_len = 4;
+bool
+test_sort_by_idx_impl(unsigned int data_num=1000, unsigned int data_len=790){
 	tensor<float,M> data(extents[data_num][data_len]);
 	tensor<unsigned int, M> indices(data_num);
 
@@ -74,8 +122,12 @@ test_sort_by_idx_impl(){
 
 	for (unsigned int i = 0; i < data_num; ++i)
 	{
-		tensor<float,M> tmp(cuv::indices[i][index_range(0,data_len)],data); 
-		tmp = (float) hidx[i];
+		//tensor<float,M> tmp(cuv::indices[i][index_range(0,data_len)],data); 
+		//tmp = (float) hidx[i];
+		for (unsigned int j = 0; j < data_len; ++j)
+		{
+			data(i,j) = (float) hidx[i];
+		}
 	}
 
 	tensor<float, M> sorted(data.shape());
@@ -113,5 +165,14 @@ BOOST_AUTO_TEST_CASE( test_sort_by_idx_dev )
 	test_sort_by_idx_impl<dev_memory_space>();
 }
 
+
+/** 
+ * @test
+ * @brief test reordering of a dataset according to indices (speed)
+ */
+BOOST_AUTO_TEST_CASE( test_sort_by_idx_speed )
+{
+	time_sort_by_idx_impl(50000,784);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
