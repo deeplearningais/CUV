098e1decf5f9ce2cb547d85eac05df2f233deba6
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-03-12 20:37:51 +0100

    commit instantiation files for easier building w/o additional dependencies

---
+++ src/cuv/tensor_ops/instantiations/inst04.cu
@@ -0,0 +1,73 @@
+
+/**************************************************
+ This is an auto-generated file.
+ See instantiate.py to modify the content in here!
+ **************************************************/
+ #include "../tensor_ops.cuh"
+ namespace cuv{
+ template bool has_inf<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template bool has_nan<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float minimum<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float maximum<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float sum<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template unsigned int count<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&, const tensor<float,host_memory_space>::value_type&);
+template float norm1<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float norm2<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float diff_norm2<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&, const tensor<float,host_memory_space>&);
+template float mean<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template float var<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template typename tensor<float,host_memory_space>::index_type     arg_max<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template typename tensor<float,host_memory_space>::index_type     arg_min<tensor<float,host_memory_space>::value_type,tensor<float,host_memory_space>::memory_space_type >(const tensor<float,host_memory_space>&);
+template bool has_inf<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template bool has_nan<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float minimum<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float maximum<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float sum<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template unsigned int count<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&, const tensor<unsigned int,host_memory_space>::value_type&);
+template float norm1<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float norm2<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float diff_norm2<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&, const tensor<unsigned int,host_memory_space>&);
+template float mean<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template float var<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template typename tensor<unsigned int,host_memory_space>::index_type     arg_max<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template typename tensor<unsigned int,host_memory_space>::index_type     arg_min<tensor<unsigned int,host_memory_space>::value_type,tensor<unsigned int,host_memory_space>::memory_space_type >(const tensor<unsigned int,host_memory_space>&);
+template bool has_inf<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template bool has_nan<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float minimum<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float maximum<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float sum<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template unsigned int count<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&, const tensor<int,host_memory_space>::value_type&);
+template float norm1<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float norm2<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float diff_norm2<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&, const tensor<int,host_memory_space>&);
+template float mean<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template float var<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template typename tensor<int,host_memory_space>::index_type     arg_max<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template typename tensor<int,host_memory_space>::index_type     arg_min<tensor<int,host_memory_space>::value_type,tensor<int,host_memory_space>::memory_space_type >(const tensor<int,host_memory_space>&);
+template bool has_inf<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template bool has_nan<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float minimum<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float maximum<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float sum<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template unsigned int count<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&, const tensor<unsigned char,host_memory_space>::value_type&);
+template float norm1<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float norm2<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float diff_norm2<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&, const tensor<unsigned char,host_memory_space>&);
+template float mean<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template float var<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template typename tensor<unsigned char,host_memory_space>::index_type     arg_max<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template typename tensor<unsigned char,host_memory_space>::index_type     arg_min<tensor<unsigned char,host_memory_space>::value_type,tensor<unsigned char,host_memory_space>::memory_space_type >(const tensor<unsigned char,host_memory_space>&);
+template bool has_inf<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template bool has_nan<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float minimum<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float maximum<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float sum<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template unsigned int count<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&, const tensor<signed char,host_memory_space>::value_type&);
+template float norm1<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float norm2<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float diff_norm2<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&, const tensor<signed char,host_memory_space>&);
+template float mean<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template float var<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template typename tensor<signed char,host_memory_space>::index_type     arg_max<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+template typename tensor<signed char,host_memory_space>::index_type     arg_min<tensor<signed char,host_memory_space>::value_type,tensor<signed char,host_memory_space>::memory_space_type >(const tensor<signed char,host_memory_space>&);
+}
