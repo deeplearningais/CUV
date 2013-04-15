b5c6932ba1731e8cac23e6dab72457a68484ff39
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-04-15 16:31:26 +0200

    add generated instantiations (they just produce problems in build otherwise :/)

---
+++ src/cuv/tensor_ops/instantiations/inst00.cu
@@ -0,0 +1,18 @@
+
+/**************************************************
+ This is an auto-generated file.
+ See instantiate.py to modify the content in here!
+ **************************************************/
+ #include "../tensor_ops.cuh"
+ namespace cuv{
+ template void apply_0ary_functor<float,host_memory_space >(tensor<float,host_memory_space>&, const NullaryFunctor&);
+template void apply_0ary_functor<float,host_memory_space >(tensor<float,host_memory_space>&, const NullaryFunctor&, const float&);
+template void apply_0ary_functor<unsigned int,host_memory_space >(tensor<unsigned int,host_memory_space>&, const NullaryFunctor&);
+template void apply_0ary_functor<unsigned int,host_memory_space >(tensor<unsigned int,host_memory_space>&, const NullaryFunctor&, const unsigned int&);
+template void apply_0ary_functor<int,host_memory_space >(tensor<int,host_memory_space>&, const NullaryFunctor&);
+template void apply_0ary_functor<int,host_memory_space >(tensor<int,host_memory_space>&, const NullaryFunctor&, const int&);
+template void apply_0ary_functor<unsigned char,host_memory_space >(tensor<unsigned char,host_memory_space>&, const NullaryFunctor&);
+template void apply_0ary_functor<unsigned char,host_memory_space >(tensor<unsigned char,host_memory_space>&, const NullaryFunctor&, const unsigned char&);
+template void apply_0ary_functor<signed char,host_memory_space >(tensor<signed char,host_memory_space>&, const NullaryFunctor&);
+template void apply_0ary_functor<signed char,host_memory_space >(tensor<signed char,host_memory_space>&, const NullaryFunctor&, const signed char&);
+}
