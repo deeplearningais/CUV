f14cab1d242969cc38351705125bda5661fd2d52
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-04-23 19:04:30 +0200

    additional hint for pyublas

--- CMakeModules/FindPyUblas.cmake
+++ CMakeModules/FindPyUblas.cmake
@@ -3,6 +3,6 @@
 execute_process ( COMMAND python -c "import pyublas; print(pyublas.__path__[0])" OUTPUT_VARIABLE PYUBLAS_OUT OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 find_path(PYUBLAS_INCLUDE_DIR pyublas/numpy.hpp
-          HINTS ${PYUBLAS_OUT}/../include /usr/local/include /usr/include)
+          HINTS ${PYUBLAS_OUT}/../include ${PYUBLAS_OUT}/include /usr/local/include /usr/include)
 
 #mark_as_advanced(PYUBLAS_INCLUDE_DIR)
