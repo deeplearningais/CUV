7d703c067b20257c6f97d5c3c7b7edb8b78a7fae
Hannes Schulz <schulz@ais.uni-bonn.de>
2014-07-01 17:03:36 +0200

    ENH export Alex Krishevskys convolution routines to python

--- src/cuv_python/export_convolution_ops.cpp
+++ src/cuv_python/export_convolution_ops.cpp
@@ -51,8 +51,221 @@ namespace ublas = boost::numeric::ublas;
 
 
 
-template <class M>
+template <class T>
 void export_convolve(){
+    typedef typename T::value_type V;
+    typedef typename T::memory_space_type M;
+    typedef typename T::memory_layout_type L;
+    typedef cuv::tensor<int,M,L> IT;
+
+    using namespace cuv::alex_conv;
+
+    def("reorder_for_conv",(void (*)(T&,const T&))reorder_for_conv<V,M,L>, (
+                arg("dst"),
+                arg("src")));
+    def("reorder_from_conv",(void (*)(T&,const T&))reorder_from_conv<V,M,L>, (
+                arg("dst"),
+                arg("src")));
+    def("convolve2d",(void (*)(T&,const T&,const T&,int, unsigned int, unsigned int, float, float)) convolve2d<V,M,L>, (
+                arg("dst"),
+                arg("img"),
+                arg("filter"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("convolve2d",(void (*)(T&,const T&,const T&,const IT&, int, unsigned int, unsigned int, float, float)) convolve2d<V,M,L>, (
+                arg("dst"),
+                arg("img"),
+                arg("filter"),
+                arg("indices"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("d_conv2d_dimg",(void (*)(T&,const T&,const T&,int, unsigned int, unsigned int, float, float)) d_conv2d_dimg<V,M,L>, (
+                arg("dst"),
+                arg("delta"),
+                arg("filter"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("d_conv2d_dimg",(void (*)(T&,const T&,const T&,const IT&,int, unsigned int, unsigned int, float, float)) d_conv2d_dimg<V,M,L>, (
+                arg("dst"),
+                arg("delta"),
+                arg("filter"),
+                arg("indices"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("d_conv2d_dfilt",(void (*)(T&,const T&,const T&,int, unsigned int, unsigned int, unsigned int, float, float)) d_conv2d_dfilt<V,M,L>, (
+                arg("dst"),
+                arg("delta"),
+                arg("input"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("partial_sum")=1,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+    def("d_conv2d_dfilt",(void (*)(T&,const T&,const T&, const IT&, int, unsigned int, unsigned int, unsigned int, float, float)) d_conv2d_dfilt<V,M,L>, (
+                arg("dst"),
+                arg("delta"),
+                arg("input"),
+                arg("indices"),
+                arg("padding_start")=0,
+                arg("module_stride")=0,
+                arg("n_groups")=0,
+                arg("partial_sum")=1,
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+
+    def("local_pool",(void (*)(T&,const T&, int, int, int, int, pool_type)) local_pool<V,M,L>, (
+                arg("dst"),
+                arg("images"),
+                arg("subsx"),
+                arg("startx"),
+                arg("stridex"),
+                arg("outputsx"),
+                arg("pool_type")));
+
+    def("local_max_pool_grad",(void (*)(T&,const T&, const T&, const T&, int, int, int, float, float)) local_max_pool_grad<V,M,L>, (
+                arg("target"),
+                arg("images"),
+                arg("maxGrads"),
+                arg("maxActs"),
+                arg("subsx"),
+                arg("startx"),
+                arg("stridex"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f));
+
+    def("local_avg_pool_grad",(void (*)(T&,const T&, int, int, int)) local_avg_pool_grad<V,M,L>, (
+                arg("target"),
+                arg("avgGrads"),
+                arg("subsx"),
+                arg("startx"),
+                arg("stridex")));
+
+    def("response_normalization",(void (*)(T&, T&, const T&, int, float, float)) response_normalization<V,M,L>, (
+                arg("target"),
+                arg("denoms"),
+                arg("images"),
+                arg("patch_size"),
+                arg("add_scale"),
+                arg("pow_scale")));
+
+    def("response_normalization_grad",(void (*)(T&, T&, const T&, const T&, const T&, int, float, float, float, float)) response_normalization_grad<V,M,L>, (
+                arg("input_gradients"),
+                arg("original_outputs"),
+                arg("original_inputs"),
+                arg("delta"),
+                arg("denoms"),
+                arg("patch_size"),
+                arg("add_scale"),
+                arg("pow_scale"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("contrast_normalization",(void (*)(T&, T&, const T&, const T&, int, float, float)) contrast_normalization<V,M,L>, (
+                arg("target"),
+                arg("denoms"),
+                arg("mean_diffs"),
+                arg("images"),
+                arg("patch_size"),
+                arg("add_scale"),
+                arg("pow_scale")));
+
+    def("contrast_normalization_grad",(void (*)(T&, T&, const T&, const T&, const T&, int, float, float, float, float)) contrast_normalization_grad<V,M,L>, (
+                arg("input_gradients"),
+                arg("original_outputs"),
+                arg("mean_diffs"),
+                arg("delta"),
+                arg("denoms"),
+                arg("patch_size"),
+                arg("add_scale"),
+                arg("pow_scale"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+
+    def("response_norm_cross_map",(void (*)(T&, T&, const T&, int, float, float, bool)) response_norm_cross_map<V,M,L>, (
+                arg("target"),
+                arg("denoms"),
+                arg("images"),
+                arg("sizeF"),
+                arg("add_scale"),
+                arg("pow_scale"),
+                arg("blocked")));
+    def("response_norm_cross_map_grad",(void (*)(T&, T&, const T&, const T&, const T&, int, float, float, bool, float, float)) response_norm_cross_map_grad<V,M,L>, (
+                arg("target"),
+                arg("denoms"),
+                arg("images"),
+                arg("sizeF"),
+                arg("add_scale"),
+                arg("pow_scale"),
+                arg("blocked"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f
+                ));
+    def("gaussian_blur",(void (*)(T&, const T&, const T&, bool, float, float)) gaussian_blur<V,M,L>, (
+                arg("target"),
+                arg("images"),
+                arg("filter"),
+                arg("horiz"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f));
+
+    def("bed_of_nails",(void (*)(T&, const T&, int, int, float, float)) bed_of_nails<V,M,L>, (
+                arg("target"),
+                arg("images"),
+                arg("start_x"),
+                arg("stride_x"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f));
+
+    def("bed_of_nails_grad",(void (*)(T&, const T&, int, int, float, float)) bed_of_nails_grad<V,M,L>, (
+                arg("target"),
+                arg("delta"),
+                arg("start_x"),
+                arg("stride_x"),
+                arg("fact_new")=1.f,
+                arg("fact_old")=0.f));
+
+    def("crop",(void (*)(T&, const T&, int, int)) crop<V,M,L>, (
+                arg("target"),
+                arg("images"),
+                arg("starty"),
+                arg("startx")));
+    def("project_to_ball", (void(*)(T&, float)) project_to_ball<V,M,L>, (
+                arg("filters"),
+                arg("ball_size")));
+
+    def("resize_bilinear", (void(*)(T&, const T&, float)) resize_bilinear<V,M,L>, (
+                arg("target"),
+                arg("images"),
+                arg("scale")));
+
+
+    
     //def("convolve",(void (*)(M&,M&, M&, int))convolve<typename M::value_type,typename M::memory_space_type, typename M::memory_layout_type>, (
     //            arg("dst"),
     //            arg("img"),
@@ -164,6 +377,10 @@ void export_rlcnp_stuff(){
 }
 
 void export_convolution_ops(){
+    enum_<cuv::alex_conv::pool_type>("pool_type")
+        .value("PT_MAX", cuv::alex_conv::PT_MAX)
+        .value("PT_AVG", cuv::alex_conv::PT_AVG)
+        ;
     export_convolve< tensor<float,host_memory_space,row_major> >();
     export_convolve< tensor<float,dev_memory_space,row_major> >();
     export_sampling_stuff< tensor<float,host_memory_space, row_major> >();
